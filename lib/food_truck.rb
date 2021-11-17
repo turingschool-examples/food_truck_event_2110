5ad3011c4df2a26d6d88cbf87e1e5aa6d041e8df
Eldridge Turambi <eturambi95@gmail.com>
2021-11-17 09:24:07 -0700

    Add code to pass exist and attribute tests for FoodTruck class

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -7,11 +7,11 @@ RSpec.describe FoodTruck do
   end
 
   it 'exists' do
-    expect(@food_truck).to be_an_instance_of(Item)
+    expect(@food_truck).to be_an_instance_of(FoodTruck)
   end
 
   it 'has attributes' do
     expect(@food_truck.name).to eq('Rocky Mountain Pies')
-    expect(@food_truck.inventrory).to eq({})
+    expect(@food_truck.inventory).to eq({})
   end
 end
