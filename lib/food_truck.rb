0da8ce485101591318b79a6e011107e6acf08c1c
Jessica Grazulis <jlgrazulis@gmail.com>
2021-11-17 09:13:05 -0700

    Add name parameter

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,4 +1,7 @@
 class Food_truck
-  def initialize
+
+  attr_reader :name
+  def initialize(name)
+    @name = name
   end
-end 
+end
--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -3,7 +3,7 @@ require './lib/food_truck'
 
 describe Food_truck do
   it 'exists' do
-    food_truck = Food_truck.new
+    food_truck = Food_truck.new("Rocky Mountain Pies")
     expect(food_truck).to be_an_instance_of(Food_truck)
   end
 
