8afe5e199f14efbb89b95fbefea1f1c3858580ab
Kevin Gloss <kgloss89@gmail.com>
2021-11-17 10:14:44 -0700

    Update variables and stock the items

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -4,13 +4,15 @@ require './lib/food_truck'
 RSpec.describe FoodTruck do
   let(:item1) {Item.new({name: 'Peach Pie (Slice)', price: '$3.75'})}
   let(:item2) {Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})}
+  let(:item3) {Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})}
+  let(:item4) {Item.new({name: "Banana Nice Cream", price: "$4.25"})}
   let(:food_truck) {FoodTruck.new("Rocky Mountain Pies")}
 
   let(:food_truck2) {FoodTruck.new("Ba-Nom-a-Nom")}
   let(:food_truck3) {FoodTruck.new("Palisade Peach Shack")}
   let(:stock_items) do
-    food_truck1.stock(item1, 35)
-    food_truck1.stock(item2, 7)
+    food_truck.stock(item1, 35)
+    food_truck.stock(item2, 7)
     food_truck2.stock(item4, 50)
     food_truck2.stock(item3, 25)
     food_truck3.stock(item1, 65)
@@ -53,6 +55,8 @@ RSpec.describe FoodTruck do
   end
 
   it 'can calculate potential revenue' do
+    stock_items
+
     expect(food_truck.potential_revenue).to eq(148.75)
     expect(food_truck2.potential_revenue).to eq(345.00)
     expect(food_truck3.potential_revenue).to eq(243.75)
