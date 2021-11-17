1579faf795b2b80b983ee7368a0234982075a475
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-17 09:30:03 -0700

    Feat: add #stock method; all tests pass

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -2,32 +2,32 @@ require './lib/item'
 require './lib/food_truck'
 
 RSpec.describe FoodTruck do
-  xit 'exists' do
+  it 'exists' do
     food_truck = FoodTruck.new("Rocky Mountain Pies")
 
     expect(food_truck).to be_an_instance_of(FoodTruck)
   end
 
-  xit 'has a name' do
+  it 'has a name' do
     food_truck = FoodTruck.new("Rocky Mountain Pies")
 
     expect(food_truck.name).to eq("Rocky Mountain Pies")
   end
 
-  xit 'starts with no inventory' do
+  it 'starts with no inventory' do
     food_truck = FoodTruck.new("Rocky Mountain Pies")
 
     expect(food_truck.inventory).to eq({})
   end
 
-  xit 'can check its stock' do
+  it 'can check its stock' do
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
 
     expect(food_truck.check_stock(item1)).to eq(0)
   end
 
-  xit 'can stock items' do
+  it 'can stock items' do
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
@@ -39,5 +39,5 @@ RSpec.describe FoodTruck do
     expect(food_truck.check_stock(item1)).to eq(55)
     food_truck.stock(item2, 12)
     expect(food_truck.inventory).to eq({item1 => 55, item2 => 12})
-  end 
+  end
 end
