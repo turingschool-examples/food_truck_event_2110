bfb9e7a8913b63bb8ffc2d3c4bb948e20b18fe23
Robin Dittrich <robinodittrich@gmail.com>
2021-11-17 08:54:34 -0800

    Complete food trucks that sell method

--- lib/event.rb
+++ lib/event.rb
@@ -1,6 +1,28 @@
 class Event
-  attr_reader :name
+  attr_reader :name, :food_trucks
   def initialize(name)
     @name = name
+    @food_trucks = []
+  end
+
+  def add_food_truck(truck)
+    @food_trucks << truck
+  end
+
+  def food_truck_names
+    @food_trucks.map do |truck|
+      truck.name
+    end
+  end
+
+  def food_trucks_that_sell(item_name)
+    trucks_selling_item = []
+    @food_trucks.each do |truck|
+      if truck.check_stock(item_name) > 0
+        trucks_selling_item << truck
+      else
+      end
+    end
+    trucks_selling_item
   end
 end
