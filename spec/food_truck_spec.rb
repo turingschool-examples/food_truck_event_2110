c2c4f6a2cfc8072cb41a6c1b663aef89b048720e
Sam Iavri <sammi80134@gmail.com>
2021-11-17 11:58:05 -0700

    Item 2 in in stock

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -31,18 +31,21 @@ RSpec.describe do
     food_truck.stock(item1, 30)
     expect(food_truck.check_stock(item1)).to eq(30)
   end
-  # food_truck.inventory
-  # {#<Item:0x007f9c56740d48...> => 30}
-  #
-  # food_truck.check_stock(item1)
-  # 30
-  #
-  # food_truck.stock(item1, 25)
-  #
-  # food_truck.check_stock(item1)
-  # 55
-  #
-  # food_truck.stock(item2, 12)
-  #
-  # food_truck.inventory
+
+  it 'has stock' do
+    food_truck = FoodTruck.new('Rocky Mountain Pies')
+    item1 = Item.new({ name: 'Peach Pie (Slice)', price: '$3.75' })
+
+    food_truck.stock(item1, 30 + 25)
+    expect(food_truck.check_stock(item1)).to eq(55)
+  end
+
+  it 'has stock' do
+    food_truck = FoodTruck.new('Rocky Mountain Pies')
+    item1 = Item.new({ name: 'Peach Pie (Slice)', price: '$3.75' })
+    item2 = Item.new({ name: 'Apple Pie (Slice)', price: '$2.50' })
+    food_truck.stock(item2, 12)
+    food_truck.stock(item1, 30 + 25)
+    expect(food_truck.inventory).to eq({ item1 => 55, item2 => 12 })
+  end
 end
