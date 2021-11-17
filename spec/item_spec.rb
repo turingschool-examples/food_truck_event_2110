e34cfa44a3fc719d3431852c6b39ecdf6e06b78f
Kevin Gloss <kgloss89@gmail.com>
2021-11-17 09:20:11 -0700

    Add test to format price to float

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -9,7 +9,11 @@ RSpec.describe Item do
   end
 
   it 'has attributes' do
-    expect(item2.name).to be_an_instance_of('Apple Pie (Slice)')
-    expect(item2.price).to be_an_instance_of(2.50)
+    expect(item2.name).to eq('Apple Pie (Slice)')
+    expect(item2.price).to eq(2.50)
+  end
+
+  it 'can format the item_price to float' do
+    expect(item2.item_price('$2.50')).to eq(2.50)
   end
 end
