f2ce7fbde4bd9fe26d8c539c4997884e8c7c3027
Kevin Gloss <kgloss89@gmail.com>
2021-11-17 10:04:34 -0700

    Update let block with additional trucks and add potential revenue test

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -6,6 +6,16 @@ RSpec.describe FoodTruck do
   let(:item2) {Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})}
   let(:food_truck) {FoodTruck.new("Rocky Mountain Pies")}
 
+  let(:food_truck2) {FoodTruck.new("Ba-Nom-a-Nom")}
+  let(:food_truck3) {FoodTruck.new("Palisade Peach Shack")}
+  let(:stock_items) do
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3.stock(item1, 65)
+  end
+
   it 'exists' do
     expect(food_truck).to be_an_instance_of(FoodTruck)
   end
@@ -40,5 +50,11 @@ RSpec.describe FoodTruck do
     food_truck.stock(item2, 12)
 
     expect(food_truck.inventory).to eq({item1 => 55, item2 => 12})
+  end
+
+  it 'can calculate potential revenue' do
+    expect(food_truck.potential_revenue).to eq(148.75)
+    expect(food_truck2.potential_revenue).to eq(345.00)
+    expect(food_truck3.potential_revenue).to eq(243.75)
   end
 end
