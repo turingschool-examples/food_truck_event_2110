9e55693069eada2e14bba64f46a5fde55ea5cb3b
David Kassin <david.j.kassin@gmail.com>
2021-11-17 09:46:45 -0700

    test: Show trucks by name

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -22,4 +22,17 @@ describe Event do
 
     expect(event.food_trucks).to eq (expected)
   end
+
+  it 'Can show all food trucks by name ' do
+    event = Event.new("South Pearl Street Farmers Market")
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    expected = ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
+
+    expect(event.food_truck_names).to eq (expected)
+  end
 end
