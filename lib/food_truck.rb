af00cb3e3a43e3ce7303bd1f25d99b812740bbf8
Sam Iavri <sammi80134@gmail.com>
2021-11-17 11:06:36 -0700

    Food truck has stock

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,8 +1,17 @@
+require './lib/item'
 class FoodTruck
   attr_reader :name, :inventory
 
   def initialize(name)
     @name = name
     @inventory = {}
+  end
+
+  def check_stock(item)
+    @inventory[item]
+  end
+
+  def stock(item, stock)
+    @inventory = { item => stock }
   end
 end
--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -11,13 +11,26 @@ RSpec.describe do
     food_truck = FoodTruck.new('Rocky Mountain Pies')
     expect(food_truck.inventory).to eq({})
   end
-  # {}
-  #
-  # food_truck.check_stock(item1)
-  # 0
-  #
-  # food_truck.stock(item1, 30)
-  #
+
+  xit 'has no stock' do
+    food_truck = FoodTruck.new('Rocky Mountain Pies')
+    item1 = Item.new({ name: 'Peach Pie (Slice)', price: '$3.75' })
+    expect(food_truck.check_stock(item1)).to eq(0)
+  end
+
+  it 'has inventory' do
+    food_truck = FoodTruck.new('Rocky Mountain Pies')
+    item1 = Item.new({ name: 'Peach Pie (Slice)', price: '$3.75' })
+    food_truck.stock(item1, 30)
+    expect(food_truck.inventory).to eq({ item1 => 30 })
+  end
+
+  it 'has stock' do
+    food_truck = FoodTruck.new('Rocky Mountain Pies')
+    item1 = Item.new({ name: 'Peach Pie (Slice)', price: '$3.75' })
+    food_truck.stock(item1, 30)
+    expect(food_truck.check_stock(item1)).to eq(30)
+  end
   # food_truck.inventory
   # {#<Item:0x007f9c56740d48...> => 30}
   #
