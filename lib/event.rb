8e440b00a6425afc6489b33878e78fd3f94fb620
Joel Grant <joelmgrant@me.com>
2021-11-17 12:00:08 -0700

    Final commit

--- lib/event.rb
+++ lib/event.rb
@@ -17,11 +17,6 @@ class Event
   end
 
   def food_trucks_that_sell(item)
-  #   trucks = @food_trucks.select do |truck|
-  #     truck.inventory.select do |food|
-  #       food.name == item.name
-  #   end
-  # end
     trucks = []
     @food_trucks.each do |truck|
       truck.inventory.each do |food|
@@ -30,8 +25,46 @@ class Event
         end
       end
     end
-    # require 'pry'; binding.pry
+    trucks
   end
 
+
+
+  def total_inventory
+    hash = {}
+    hash_1 = {}
+    hash_1[:quantity] = 0
+    hash_1[:food_trucks] = []
+    inventory = @food_trucks.flat_map { |truck| truck.inventory.keys }.uniq
+    inventory.each do |item|
+      count = 0
+      food_trucks_that_sell(item).each do |truck|
+        truck.inventory.each do |food|
+          if food[0].name == item.name
+            count += food[1]
+          end
+          hash_1[:quantity] = 7
+        end
+        hash_1[:food_trucks] = food_trucks_that_sell(item)
+        hash[item] = hash_1
+        # require 'pry'; binding.pry
+      end
+
+    end
+    hash
+    require 'pry'; binding.pry
+  end
+
+  def overstocked_items
+
+  end
+
+  def sorted_item_list
+    list = @food_trucks.flat_map do |truck|
+      truck.inventory.map do |item|
+        item[0].name
+      end
+    end.sort.uniq
+  end
 
 end
