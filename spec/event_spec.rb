b1b9873bfa362fbcce0181129b76ead650175135
Jessica Grazulis <jlgrazulis@gmail.com>
2021-11-17 09:47:25 -0700

    Add name test

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -7,4 +7,9 @@ describe Event do
     event = Event.new("South Pearl Street Farmers Market")
     expect(event).to be_an_instance_of(Event)
   end
+
+  it 'has attributes' do
+    event = Event.new("South Pearl Street Farmers Market")
+    expect(event.name).to eq("South Pearl Street Farmers Market")
+  end
 end
