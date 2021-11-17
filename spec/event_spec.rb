cb00c7b5e2dbabae6ef814b11702945bb80aa919
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2021-11-17 09:57:59 -0700

    Test: #food_truck_names

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -6,8 +6,6 @@ require './lib/event'
 # pry(main)> food_truck2.stock(item4, 50)
 # pry(main)> food_truck2.stock(item3, 25)
 # pry(main)> food_truck3.stock(item1, 65)
-# pry(main)> event.food_trucks
-# #=> [#<FoodTruck:0x00007fe1348a1160...>, #<FoodTruck:0x00007fe1349bed40...>, #<FoodTruck:0x00007fe134910650...>]
 # pry(main)> event.food_truck_names
 # #=> ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
 # pry(main)> event.food_trucks_that_sell(item1)
@@ -53,6 +51,12 @@ RSpec.describe Event do
       event.add_food_truck(food_truck2)
       event.add_food_truck(food_truck3)
       expect(event.food_trucks).to eq [food_truck1, food_truck2, food_truck3]
+    end
+  end
+
+  describe '#food_truck_names' do
+    it "returns the names of the trucks at the event" do
+      expect(event.food_truck_names).to eq ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
     end
   end
 end
