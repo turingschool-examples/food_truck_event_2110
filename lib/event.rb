bb243f3f34e7c970d761f49a394e993ad11c8fa3
Paul Leonard <paulpaulleonard@gmail.com>
2021-11-17 11:50:43 -0500

    Pass attributes test

--- lib/event.rb
+++ lib/event.rb
@@ -2,10 +2,11 @@ require './lib/item'
 require './lib/food_truck'
 
 class Event
-
+  attr_reader :name, :food_trucks
 
   def initialize(name)
-    
+    @name        = name
+    @food_trucks = []
   end
 
 end
