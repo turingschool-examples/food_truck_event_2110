9914d0d858fc0b17c20d42c3272c28a3c648ba3b
Jessica Grazulis <jlgrazulis@gmail.com>
2021-11-17 10:21:18 -0700

    Add #vendors_that_sell

--- lib/event.rb
+++ lib/event.rb
@@ -12,8 +12,18 @@ class Event
   def food_truck_names
     truck_names = []
     food_trucks.each do |food_truck|
-      truck_names << food_truck.name 
+      truck_names << food_truck.name
     end
     truck_names
+  end
+
+  def food_trucks_that_sell(item)
+    selling_food_trucks = []
+    food_trucks.each do |food_truck|
+      if food_truck.inventory.include?(item) == true
+        selling_food_trucks << food_truck
+      end
+    end
+    selling_food_trucks
   end
 end
