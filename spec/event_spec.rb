6f39bd638d22bedfc991618e628b22d23760e456
Parker <parkerlockhart42@gmail.com>
2021-11-17 10:43:26 -0600

    Test: add_food_truck

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -12,6 +12,11 @@ describe Event do
     @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @food_truck1.stock(item1, 35)
+    @food_truck1.stock(item2, 7)
+    @food_truck2.stock(item4, 50)
+    @food_truck2.stock(item3, 25)
+    @food_truck3.stock(item1, 65)
   end
 
   describe '#initialize' do
@@ -26,6 +31,15 @@ describe Event do
     it 'has food_trucks' do
       expect(@event.food_trucks).to eq([])
     end
-  end 
+  end
+
+  describe 'add_food_truck' do
+    it 'adds food trucks to array' do
+      @event.add_food_truck(food_truck1)
+      @event.add_food_truck(food_truck2)
+      @event.add_food_truck(food_truck3)
+      expect(@event.food_trucks).to eq([@food_truck1, @food_truck2, @food_truck3])
+    end
+  end
 
 end
