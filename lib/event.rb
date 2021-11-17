da0c62cb1592ea645e0af187a9292ff623c652fe
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-17 10:37:35 -0700

    Feat: add #overstocked_items method; test passes

--- lib/event.rb
+++ lib/event.rb
@@ -35,4 +35,18 @@ class Event
     end
     total
   end
+
+  def overstocked_items
+    #An item is overstocked if it is sold by more than 1
+    #food truck AND the total quantity is greater than 50.
+    overstocked = []
+    @food_trucks.each do |food_truck|
+      food_truck.inventory.each do |item, quantity|
+        if food_trucks_that_sell(item).length > 1 && quantity >50
+          overstocked << item
+        end
+      end
+    end
+    overstocked
+  end
 end
