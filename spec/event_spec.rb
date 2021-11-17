b5230e94ccb500a11ebe8321395eab12a753704f
David Kassin <david.j.kassin@gmail.com>
2021-11-17 11:17:20 -0700

    testL Show can create has of each item quantity and who sells it

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -171,4 +171,31 @@ describe Event do
     expect(event.item_quant(item3)).to eq 35
     expect(event.item_quant(item4)).to eq 50
   end
+
+  it 'Can hash for each item quantity and who sells' do
+    event = Event.new("South Pearl Street Farmers Market")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck3.stock(item1, 65)
+    food_truck3.stock(item3, 10)
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    expected =
+    {
+      quantity: 100 ,
+      food_trucks: [food_truck1, food_truck3]
+    }
+    expect(event.item_details(item1)).to eq (expected)
+
+  end
 end
