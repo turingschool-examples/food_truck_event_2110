3cf13a00467a25cbc8b9a03f18c12476febcb50f
Katy Harrod <m.catherine.harrod@gmail.com>
2021-11-17 09:50:07 -0700

    Pass stock spec

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,7 +1,20 @@
 class FoodTruck
-  attr_reader :name
-  
+  attr_reader :name,
+              :inventory
+
   def initialize(name)
     @name = name
+    @inventory = Hash.new(0)
   end
+
+  def check_stock(item)
+    # return 0 unless @inventory.has_key?(item)
+    @inventory[item]
+  end
+
+  def stock(item, amount)
+    @inventory[item] += amount
+  end
+
+  
 end
