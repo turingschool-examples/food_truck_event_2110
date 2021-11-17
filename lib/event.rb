50b4584f0a4109e7151b6a8ff81b8c0fe668b8ea
Chris Hewitt <agop5134@gmail.com>
2021-11-17 11:16:12 -0700

    Iteration 3

--- lib/event.rb
+++ lib/event.rb
@@ -22,4 +22,38 @@ class Event
     end
   end
 
-end
+  def sorted_item_list
+    @food_trucks.map do |truck|
+      truck.inventory.map do |item, quant|
+        item.name
+      end
+    end.flatten.sort.uniq
+  end
+
+  def overstocked_items
+    overstock = []
+    @food_trucks.each do |truck|
+      truck.inventory.each do |item, quant|
+        if quant > 50 && food_trucks_that_sell(item).count > 1
+          overstock << item
+          end
+        end
+      end
+    overstock
+  end
+
+  def total_inventory
+    total_inventory_hash = Hash.new(0)
+    @food_trucks.each do |truck|
+      truck.inventory.each do |item, quant|
+        if total_inventory_hash.include?(item) == false
+          total_inventory_hash[item] = {:quantity => 0, :food_trucks => food_trucks_that_sell(item)}
+          total_inventory_hash[item][:quantity] += quant
+        else total_inventory_hash[item][:quantity] += quant
+        end
+      end
+    end
+    total_inventory_hash
+  end
+
+  end
