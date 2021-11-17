89ce60f797a35cfd2dfd0834f3cfffe08edf22c3
Wade Naughton <wadenaughton@gmail.com>
2021-11-17 12:01:24 -0500

    Food truck initial tests

---
+++ spec/event_spec.rb
@@ -0,0 +1,17 @@
+require './lib/item'
+require './lib/food_truck'
+require './lib/event'
+
+RSpec.describe Event do
+  it "exists" do
+    event = Event.new("South Pearl Street Farmers Market")
+    expect(event).to be_instance_of(Event)
+  end
+
+  it "has attributes" do
+    event = Event.new("South Pearl Street Farmers Market")
+    expect(event.name).to eq("South Pearl Street Farmers Market")
+    expect(event.food_trucks).to eq([])
+
+  end
+end
--- spec/foodtruck_spec.rb
+++ spec/foodtruck_spec.rb
@@ -21,6 +21,7 @@ RSpec.describe FoodTruck do
 
   it "stocks" do
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     food_truck.stock(item1, 30)
 
@@ -29,6 +30,11 @@ RSpec.describe FoodTruck do
     food_truck.stock(item1, 25)
 
     expect(food_truck.inventory).to eq({item1 => 55})
+
+    food_truck.stock(item2, 12)
+
+    expect(food_truck.inventory).to eq({item1 => 55, item2 => 12})
+    expect(food_truck.check_stock(item2)).to eq(12)
   end
 
 end
