e9d9294f2ffc681bd731a65237dd0c536ed7a2e2
Katy Harrod <m.catherine.harrod@gmail.com>
2021-11-17 11:44:15 -0700

    Add sort items spec

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -93,5 +93,76 @@ RSpec.describe Event do
     @event.add_food_truck(food_truck3)
 
     expect(food_truck1.potential_revenue).to eq(148.75)
+  end
+
+  it 'sorts names of foods' do
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck3.stock(item1, 65)
+    food_truck3.stock(item3, 10)
+
+    @event.add_food_truck(food_truck1)
+    @event.add_food_truck(food_truck2)
+    @event.add_food_truck(food_truck3)
+
+    expect(@event.sorted_item_list).to eq(["Apple Pie (Slice)", "Banana Nice Cream", "Peach Pie (Slice)", "Peach-Raspberry Nice Cream"])
   end 
+
+
+
+  xit '#total_inventory' do
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck3.stock(item1, 65)
+    food_truck3.stock(item3, 10)
+
+    @event.add_food_truck(food_truck1)
+    @event.add_food_truck(food_truck2)
+    @event.add_food_truck(food_truck3)
+
+    # alphabetize by item name
+    expected = {
+                item2 => { # apple
+                quantity: 7,
+                food_trucks: [food_truck1]},
+
+                item4 => { # banana
+                quantity: 35,
+                food_trucks: [food_truck2]},
+
+                item1 => { # peach p
+                quantity: 100,
+                food_trucks: [food_truck1, food_truck3]},
+
+                item3 => { # peach r
+                quantity: 50,
+                food_trucks: [food_truck2, food_truck3]}
+                }
+
+    expect(@event.something_else). to eq(expected)
+  end
 end
