e241c05b740604f13d5c3eeaa97c71800a29c7cb
Jessica Grazulis <jlgrazulis@gmail.com>
2021-11-17 09:11:14 -0700

    Add test for food truck name

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -3,7 +3,14 @@ require './lib/food_truck'
 
 describe Food_truck do
   it 'exists' do
-    food_truck = Food_truck.new 
+    food_truck = Food_truck.new
     expect(food_truck).to be_an_instance_of(Food_truck)
   end
+
+  it 'has a name' do
+    food_truck = Food_truck.new("Rocky Mountain Pies")
+    expect(food_truck.name).to eq("Rocky Mountain Pies")
+  end
+
+
 end
