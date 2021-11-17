efc5086d64f9ae66ec35210c174c197a1a0ddced
Jessica Grazulis <jlgrazulis@gmail.com>
2021-11-17 11:12:13 -0700

    Fix error in test, add #total_inventory

--- lib/event.rb
+++ lib/event.rb
@@ -26,4 +26,20 @@ class Event
     end
     selling_food_trucks
   end
+
+  def total_inventory
+    inventory_total = {}
+    food_trucks.each do |food_truck|
+      food_truck.inventory.each do |item|
+        if inventory_total[item[0]].nil? # if the hash with item as the key doesn't have a value
+          inventory_total[item[0]] = {quantity: item[1], food_trucks: [food_truck] } # add it to the hash as a value
+        else
+          inventory_total[item[0]][:quantity] += item[1]  # if it already has a value,
+          inventory_total[item[0]][:food_trucks] += [food_truck] # increase the existing values
+        end
+      end
+    end
+    inventory_total
+  end
+
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -87,7 +87,7 @@ describe Event do
     expected ={
       item1 => {
         quantity: 100,
-        food_trucks: [food_truck1, food_truck2]
+        food_trucks: [food_truck1, food_truck3]
       },
       item2 => {
         quantity: 7,
