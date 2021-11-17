bf7b949325fac99fd1fe942a3940f6c4e7c61d4b
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-17 10:32:33 -0700

    Test; add test for #overstocked_items

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -69,26 +69,19 @@ RSpec.describe Event do
     expect(event.food_trucks_that_sell(item4)).to eq([food_truck2])
   end
 
-  xit 'can list its total inventory' do
+  it 'can list its total inventory' do
     event = Event.new("South Pearl Street Farmers Market")
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-    #=> #<Item:0x007f9c56740d48...>
     item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-    #=> #<Item:0x007f9c565c0ce8...>
     item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-    #=> #<Item:0x007f9c562a5f18...>
     item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-    #=> #<Item:0x007f9c56343038...>
     food_truck1 = FoodTruck.new("Rocky Mountain Pies")
-    #=> #<FoodTruck:0x00007fe1348a1160...>
     food_truck1.stock(item1, 35)
     food_truck1.stock(item2, 7)
     food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
-    #=> #<FoodTruck:0x00007fe1349bed40...>
     food_truck2.stock(item4, 50)
     food_truck2.stock(item3, 25)
     food_truck3 = FoodTruck.new("Palisade Peach Shack")
-    #=> #<FoodTruck:0x00007fe134910650...>
     food_truck3.stock(item1, 65)
     food_truck3.stock(item3, 10)
     event.add_food_truck(food_truck1)
@@ -116,5 +109,34 @@ RSpec.describe Event do
         }
 
     expect(event.total_inventory).to eq(expected)
+  end
+
+  xit 'can list overstocked items' do
+    event = Event.new("South Pearl Street Farmers Market")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    #=> #<Item:0x007f9c56740d48...>
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    #=> #<Item:0x007f9c565c0ce8...>
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    #=> #<Item:0x007f9c562a5f18...>
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    #=> #<Item:0x007f9c56343038...>
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    #=> #<FoodTruck:0x00007fe1348a1160...>
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    #=> #<FoodTruck:0x00007fe1349bed40...>
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    #=> #<FoodTruck:0x00007fe134910650...>
+    food_truck3.stock(item1, 65)
+    food_truck3.stock(item3, 10)
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+
+    expect(event.overstocked_items).to eq([item1])
   end
 end
