bcc3cbbd61c1c0b9d3488016ddd30b6cb2ddc58d
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-17 09:05:07 -0700

    Add item init tests

---
+++ spec/item_spec.rb
@@ -0,0 +1,23 @@
+require './lib/item'
+
+RSpec.describe Item do
+
+  before(:each) do
+    @item = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+  end
+
+
+  it 'exists' do
+    expect(@item).to be_instance_of(Item)
+  end
+
+  it 'has a name' do
+    expect(@item.name).to eq("Apple Pie (Slice)")
+  end
+
+  it 'has a price' do
+    expect(@item.price).to eq(2.50)
+  end
+
+
+end
