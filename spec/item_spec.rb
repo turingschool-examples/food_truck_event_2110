b7617ad98cbb3d934508c44d6396bcdcf280d48d
Eldridge Turambi <eturambi95@gmail.com>
2021-11-17 09:18:22 -0700

    Add test for item attributes

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -11,4 +11,9 @@ RSpec.describe Item do
     expect(@item1).to be_an_instance_of(Item)
     expect(@item2).to be_an_instance_of(Item)
   end
+
+  it 'has attributes' do
+    expect(@item2.name).to eq('Apple Pie (Slice)')
+    expect(@item2.price).to eq(2.50)
+  end
 end
