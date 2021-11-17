83953a333ef3806819dcff2d26d0ddd0a428a16b
Katy Harrod <m.catherine.harrod@gmail.com>
2021-11-17 09:49:51 -0700

    Add food truck stock spec

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -4,23 +4,40 @@ require './lib/food_truck'
 
 RSpec.describe FoodTruck do
 
-  it 'exists' do
-     food_truck = FoodTruck.new("Rocky Mountain Pies")
+  before :each do
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
 
-     expect(food_truck).to be_a(FoodTruck)
-     expect(food_truck.name).to eq("Rocky Mountain Pies")
+  end
+
+  it 'exists' do
+
+     expect(@food_truck).to be_a(FoodTruck)
+     expect(@food_truck.name).to eq("Rocky Mountain Pies")
   end
 
   it 'has no inventory by default' do
-    food_truck = FoodTruck.new("Rocky Mountain Pies")
 
-    expect(food_truck.inventory).to eq({})
+    expect(@food_truck.inventory).to eq({})
+  end
 
+  it 'stocks items' do
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
 
-    expect(food_truck.check_stock(item1)).to eq(0)
-  end
+    expect(@food_truck.check_stock(item1)).to eq(0)
 
+    @food_truck.stock(item1, 30)
+
+    expect(@food_truck.inventory).to eq({item1 => 30})
+    expect(@food_truck.check_stock(item1)).to eq(30)
+
+    @food_truck.stock(item1, 25)
+
+    expect(@food_truck.check_stock(item1)).to eq(55)
+
+    @food_truck.stock(item2, 12)
+
+    expect(@food_truck.inventory).to eq({item1 => 55, item2 => 12})
+  end
 
 end
