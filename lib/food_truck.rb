66e6829584796b609d14bdc91ca563f865e6376c
Mallory Vining <mallorygallery12@gmail.com>
2021-11-17 09:49:33 -0700

    Add potential revenue

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -13,4 +13,10 @@ class FoodTruck
   def stock(item, count)
     @inventory[item] += count
   end
+
+  def potential_revenue
+    @inventory.sum do |item, count|
+      item.price * count
+    end
+  end
 end
