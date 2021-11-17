c9670926914662f3c593cd22eefbb2e1b63597ba
John Napier <jcee.napier@gmail.com>
2021-11-17 09:14:07 -0700

    Add food truck spec tests

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,37 @@
+require 'rspec'
+require './lib/item'
+require './lib/food_truck'
+
+describe FoodTruck do 
+  let(:food_truck) {FoodTruck.new("Rocky Mountain Pies")}
+  let(:item1) {Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})}
+  let(:item2) {Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})}
+
+  it 'exists' do
+    expect(food_truck).to be_an_instance_of(FoodTruck)
+  end
+
+  it 'attributes' do
+    expect(food_truck.name).to eq("Rocky Mountain Pies")
+    expect(food_truck.food_truck.inventory).to eq({})
+  end
+
+  it '#check stock' do 
+    expect(food_truck.check_stock(item1)).to eq(0)
+  end
+
+  it '#stock' do 
+    food_truck.stock(item1, 30)
+
+    expect(food_truck.food_truck.inventory).to eq({item1 => 30})
+    expect(food_truck.check_stock(item1)).to eq(30)
+
+    food_truck.stock(item1, 25)
+
+    expect(food_truck.check_stock(item1)).to eq(55)
+
+    food_truck.stock(item2, 12)
+
+    expect(food_truck.food_truck.inventory).to eq({item1 => 30, item2 => 12})
+  end
+end
