892048febdb74b0fab4dfc465c63968a01820346
Robin Dittrich <robinodittrich@gmail.com>
2021-11-17 09:30:54 -0800

    Create helper methods for total inventory log

--- lib/event.rb
+++ lib/event.rb
@@ -25,4 +25,29 @@ class Event
     end
     trucks_selling_item
   end
+
+  def keys
+    all_items = []
+    @food_trucks.each do |truck|
+      all_items << truck.inventory.keys
+    end
+    keys = all_items.flatten.uniq
+  end
+
+  def total_quantity(item_name)
+    total = 0
+    @food_trucks.each do |truck|
+      total += truck.check_stock(item_name)
+    end
+    total
+  end
+
+  def total_inventory
+    keys.reduce({}) do |key, item_name|
+      key[item_name] = {}
+      require "pry"; binding.pry
+      key[item_name][:quantity] = total_quantity(item_name)
+      key[item_name][:food_trucks] = food_trucks_that_sell(item_name)
+    end
+  end
 end
