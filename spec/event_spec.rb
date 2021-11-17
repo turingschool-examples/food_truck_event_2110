407392285a3b8456d7444ff20ebbe90c94443bd5
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-17 09:42:52 -0700

    Add initialize tests, #add_food truck/#food_truck_names/#food_trucks_that_sell method tests

---
+++ spec/event_spec.rb
@@ -0,0 +1,65 @@
+require './lib/item'
+require './lib/food_truck'
+require './lib/event'
+
+RSpec.describe Event do
+
+  before(:each) do
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    @food_truck3 = FoodTruck.new("Palisade Peach Shack")
+
+    @food_truck1.stock(item1, 35)
+    @food_truck1.stock(item2, 7)
+
+    @food_truck2.stock(item4, 50)
+    @food_truck2.stock(item3, 25)
+
+    @food_truck3.stock(item1, 65)
+
+    @event = Event.new("South Pearl Street Farmers Market")
+  end
+
+  it 'exists' do
+    expect(@event).to be_instance_of(Event)
+  end
+
+  it 'has a name' do
+    expect(@event.name).to eq("South Pearl Street Farmers Market")
+  end
+
+  it 'initializes empty @food_trucks attributes' do
+    ex pect(@event.food_trucks).to eq([])
+  end
+
+  it 'can #add_food_truck to @food_trucks[]' do
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck1)
+
+    expected_array = (@food_truck1, @food_truck2, @food_truck3)
+  end
+
+  it 'returns all #food_truck_names' do
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck1)
+
+    expected_array = ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
+    expect(@event.food_truck_names).to eq(expected_array)
+  end
+
+  it 'returns #food_trucks_that_sell(item_obj)' do
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck1)
+
+
+  end
+
+end
