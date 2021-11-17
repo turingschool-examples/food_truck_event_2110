5399b377b2ebf77ed3c52004f4201b25edab7a3c
Mallory Vining <mallorygallery12@gmail.com>
2021-11-17 09:08:06 -0700

    Initialize Item

---
+++ spec/item_spec.rb
@@ -0,0 +1,17 @@
+require "rspec"
+require "./lib/item"
+
+describe Item do
+  it 'exists' do
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    expect(item1).to be_an_instance_of(Item)
+  end
+
+  it 'attributes' do
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    expect(item2.name).to eq("Apple Pie (Slice)")
+    expect(item2.price).to eq(2.50)
+  end
+end
