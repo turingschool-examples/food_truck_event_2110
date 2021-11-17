b7c9a04406cb705275d6ac59af4f197a1d76d952
Hannah Warren <hannahkwarren@gmail.com>
2021-11-17 11:15:45 -0500

    Add check_stock method and test

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe FoodTruck do
 
   before(:each) do
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
   end
 
   it 'exists' do
@@ -13,7 +14,11 @@ RSpec.describe FoodTruck do
 
   it 'has attributes' do
     expect(@food_truck.name).to eq("Rocky Mountain Pies")
-    expect(@food_truck.inventory).to eq([])
+    expect(@food_truck.inventory).to eq({})
+  end
+
+  it '#check_stock' do
+    expect(@food_truck.check_stock(@item1)).to eq(0)
   end
 
 end
