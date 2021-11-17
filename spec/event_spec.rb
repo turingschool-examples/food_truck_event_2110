0796696f6740246cbdbe0a11ccdd1b649af5b87d
Jessica Grazulis <jlgrazulis@gmail.com>
2021-11-17 10:48:49 -0700

    Add long setup for #total_inventory

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -65,5 +65,46 @@ describe Event do
     expect(event.food_trucks_that_sell(item1)).to eq([food_truck1, food_truck3])
     expect(event.food_trucks_that_sell(item4)).to eq([food_truck2])
   end
-    
+
+  it 'shows #total_inventory' do
+    event = Event.new("South Pearl Street Farmers Market")
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3.stock(item1, 65)
+    food_truck3.stock(item3, 10)
+    expected ={
+      item1 => {
+        quantity: 100,
+        food_trucks: [food_truck1, food_truck2]
+      },
+      item2 => {
+        quantity: 7,
+        food_trucks: [food_truck1]
+      },
+
+      item3 => {
+        quantity: 35,
+        food_trucks: [food_truck2, food_truck3]
+      },
+
+      item4 => {
+        quantity: 50,
+        food_trucks: [food_truck2]
+      }
+    }
+    expect(event.total_inventory).to eq(expected)
+
+  end
 end
