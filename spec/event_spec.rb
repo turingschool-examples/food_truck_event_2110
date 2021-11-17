120d289ab062024510ce6f7a51c4e9ab4b392fd5
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-17 09:49:38 -0700

    MORE TYPOS.

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -14,8 +14,8 @@ RSpec.describe Event do
     @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
     @food_truck3 = FoodTruck.new("Palisade Peach Shack")
 
-    @food_truck1.stock(@item1, 35)
-    @food_truck1.stock(@item2, 7)
+    @food_truck.stock(@item1, 35)
+    @food_truck.stock(@item2, 7)
 
     @food_truck2.stock(@item4, 50)
     @food_truck2.stock(@item3, 25)
@@ -34,7 +34,7 @@ RSpec.describe Event do
   end
 
   it 'initializes empty @food_trucks attributes' do
-    ex pect(@event.food_trucks).to eq([])
+    expect(@event.food_trucks).to eq([])
   end
 
   it 'can #add_food_truck to @food_trucks[]' do
