1e740cd3b6763d42a7893ce0da7e5f6135966ab2
Katy Harrod <m.catherine.harrod@gmail.com>
2021-11-17 09:26:24 -0700

    Add check stock spec

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -10,11 +10,17 @@ RSpec.describe FoodTruck do
      expect(food_truck).to be_a(FoodTruck)
      expect(food_truck.name).to eq("Rocky Mountain Pies")
   end
-end
 
-#
-# item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-# #=> #<Item:0x007f9c56740d48...>
-#
-# item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-# #=> #<Item:0x007f9c565c0ce8...>
+  it 'has no inventory by default' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    expect(food_truck.inventory).to eq({})
+
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+    expect(food_truck.check_stock(item1)).to eq(0)
+  end
+
+
+end
