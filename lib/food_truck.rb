38f203ead090c943cbe246eb8a81773575bfd370
Jessica Grazulis <jlgrazulis@gmail.com>
2021-11-17 09:31:12 -0700

    Change hash to default value 0, add check stock method

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -3,6 +3,10 @@ class Food_truck
   attr_reader :name , :inventory
   def initialize(name)
     @name = name
-    @inventory = {}
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    @inventory[item]
   end
 end
