c6a4fa14fdb2482f21ec4dfa371966a013a03117
Kevin Gloss <kgloss89@gmail.com>
2021-11-17 09:20:27 -0700

    Add initialize and item_price methods

--- lib/item.rb
+++ lib/item.rb
@@ -1,1 +1,13 @@
+class Item
+  attr_reader :name, :price
 
+  def initialize(attributes)
+    @name = attributes[:name]
+    @price = item_price(attributes[:price])
+  end
+
+  def item_price(price)
+    price.slice!(0)
+    price.to_f.round(2)
+  end
+end
