11b4108c3544da56a50d06b4a963e790911f243d
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-11-17 09:12:07 -0700

    Add init test for FoodTruck

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,18 @@
+require './lib/food_truck'
+
+
+describe FoodTruck do
+
+  before(:each) do
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
+  end
+
+  describe '#initialize' do
+    it 'has attributes' do
+      expect(@food_truck.name).to eq("Rocky Mountain Pies")
+      expect(@food_truck.inventory).to eq({})
+    end
+  end
+
+
+end
