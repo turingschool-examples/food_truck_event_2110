fe7e0ebd99858b630f7bfcf82c201104310896e5
John Napier <jcee.napier@gmail.com>
2021-11-17 09:32:38 -0700

    Add #add food truck to event spec

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -35,19 +35,19 @@ describe Event do
     end
 
     it '#food trucks' do 
-      expect(event.food_trucks).to eq(food_truck1, food_truck2, food_truck3)
+      expect(event.food_trucks).to eq([food_truck1, food_truck2, food_truck3])
     end
 
-    it '#food truck names' do 
+    xit '#food truck names' do 
       expect(event.food_truck_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
     end
 
-    it '#food trucks that sell' do 
+    xit '#food trucks that sell' do 
       expect(event.food_trucks_that_sell(item1)).to eq([food_truck1, food_truck3])
       expect(event.food_trucks_that_sell(item4)).to eq([food_truck2])
     end
 
-    it '#potential revenue' do 
+    xit '#potential revenue' do 
       expect(food_truck1.potential_revenue).to eq(148.75)
       expect(food_truck2.potential_revenue).to eq(345.00)
       expect(food_truck3.potential_revenue).to eq(243.75)
