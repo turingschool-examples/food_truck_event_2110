d60c998e4934a084dca3919289de16e884050732
David Kassin <david.j.kassin@gmail.com>
2021-11-17 09:18:43 -0700

    feat: Add initialize for item stock to default to 0 and be attr_reader

--- lib/item.rb
+++ lib/item.rb
@@ -1,10 +1,11 @@
 class Item
 
-  attr_reader :name, :price
+  attr_reader :name, :price, :stock
 
   def initialize(details)
     @name = details[:name]
     @price = details[:price].delete("$").to_f
+    @stock = 0
   end
 
 end
