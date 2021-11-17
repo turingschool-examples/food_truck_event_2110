d50737e2aea6d8292c0c9ace9667e03708236824
Parker <parkerlockhart42@gmail.com>
2021-11-17 11:52:25 -0600

    Add sorted_item_list method

--- lib/event.rb
+++ lib/event.rb
@@ -16,6 +16,20 @@ class Event
   def food_trucks_that_sell(item)
     @food_trucks.select do |food_truck|
       food_truck.inventory.include?(item)
-    end 
+    end
+  end
+
+  def sorted_item_list
+    all_inventory = []
+    @food_trucks.each do |food_truck|
+      food_truck.inventory.each do |item|
+        all_inventory << item
+      end
+    end
+    alphabetically = []
+    all_inventory.sort_by do |item|
+      alphabetically << item[0].name
+    end
+    alphabetically.uniq
   end
 end
