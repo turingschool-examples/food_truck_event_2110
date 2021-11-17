14d14f19fb72130aca58819ed7ba492fcf55c36a
Wade Naughton <wadenaughton@gmail.com>
2021-11-17 12:08:05 -0500

    adds food trucks

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -12,6 +12,16 @@ RSpec.describe Event do
     event = Event.new("South Pearl Street Farmers Market")
     expect(event.name).to eq("South Pearl Street Farmers Market")
     expect(event.food_trucks).to eq([])
+  end
 
+  it "adds food truck" do
+    event = Event.new("South Pearl Street Farmers Market")
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    expect(event.food_trucks).to eq([food_truck1, food_truck2, food_truck3])
   end
 end
