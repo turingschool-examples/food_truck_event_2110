fd521460a097633605492afd50a4d5592af73141
Paul Leonard <paulpaulleonard@gmail.com>
2021-11-17 11:30:21 -0500

    write tests for food truck and item.  initiliaze Item

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,21 @@
+require './lib/item'
+require './lib/food_truck'
+
+RSpec.describe 'food truck' do
+  before(:each) do
+    @item1      = Item.new({name: 'Peach Pie (Slice)',
+                            price: "$3.75"})
+    @item2      = Item.new({name: 'Apple Pie (Slice)',
+                            price: '$2.50'})
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
+  end
+
+  it "exists" do
+    expect(@food_truck).to be_a FoodTruck
+  end
+
+  xit "has attributes that are readable" do
+    expect(@food_truck.name).to eq("Rocky Mountain Pies")
+    expect(@food_truck.inventory).to eq({})
+  end
+end
