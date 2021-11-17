8f0542f77466c68c8a2a2868a22372da0bf029a2
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-11-17 09:39:50 -0700

    Add #food_trucks_that_sell test

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -47,4 +47,13 @@ describe Event do
     end
   end
 
+  describe '#food_trucks_that_sell' do
+    it 'returns an array of trucks that sell given item' do
+      @event.add_food_truck(@food_truck1)
+      @event.add_food_truck(@food_truck2)
+      @event.add_food_truck(@food_truck3)
+      expect(@event.food_trucks_that_sell(@item1)).to eq(@food_truck1, @food_truck3)
+    end
+  end
+
 end
