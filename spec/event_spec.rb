914b0e90c6e30f8ada83968564e8f95bc60b866e
David Kassin <david.j.kassin@gmail.com>
2021-11-17 09:42:45 -0700

    test: Show can add food trucks to food__trucks instance variable

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -9,4 +9,17 @@ describe Event do
     expect(event.name).to eq ("South Pearl Street Farmers Market")
     expect(event.food_trucks).to eq ([])
   end
-end 
+
+  it 'Can show all food trucks ' do
+    event = Event.new("South Pearl Street Farmers Market")
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    expected = [food_truck1,food_truck2,food_truck3]
+
+    expect(event.food_trucks).to eq (expected)
+  end
+end
