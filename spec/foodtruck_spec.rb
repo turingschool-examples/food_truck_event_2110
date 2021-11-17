c94fafc74bb589819c0f9ed4a93c9f0326c53389
Wade Naughton <wadenaughton@gmail.com>
2021-11-17 11:30:42 -0500

    food truck inital tests

---
+++ spec/foodtruck_spec.rb
@@ -0,0 +1,16 @@
+require './lib/item'
+require './lib/food_truck'
+
+RSpec.describe FoodTruck do
+  it "exists" do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    expect(food_truck).to be_instance_of(FoodTruck)
+  end
+
+  it "has attributes" do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    expect(food_truck.name).to eq("Rocky Mountain Pies")
+    expect(food_truck.inventory).to eq({})
+  end
+
+end
