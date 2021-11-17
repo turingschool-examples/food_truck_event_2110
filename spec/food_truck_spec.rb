3c87e305fccbb2f9a1142fde3bcb192db0531794
David Kassin <david.j.kassin@gmail.com>
2021-11-17 09:29:06 -0700

    test: Show can stock and item and that can check stock of item

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -8,4 +8,27 @@ describe FoodTruck do
     expect(food_truck.name).to eq ("Rocky Mountain Pies")
     expect(food_truck.inventory).to eq ({})
   end
+
+  it 'Can show the stock of an item'  do
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    expect(food_truck.check_stock(item1)).to eq 0
+
+    food_truck.stock(item1, 30)
+
+    expect(food_truck.check_stock(item1)).to eq 30
+
+  end
+
+  it 'Can stock an item'  do
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    food_truck.stock(item1, 30)
+    expected = {item1 => 30}
+
+    expect(food_truck.inventory).to eq (expected)
+  end
 end
