f202452ef6320c1cd875677ee337459f751b5d95
Eric Chon <echon92@gmail.com>
2021-11-17 09:37:04 -0700

    completed iteration1

---
+++ lib/food_truck.rb
@@ -0,0 +1,28 @@
+class Foodtruck
+  attr_reader :name, :inventory
+
+  def initialize(name)
+    @name = name
+    @inventory = {}
+  end
+
+  def check_stock(item)
+    stock_of_item = 0
+    @inventory.each do |things , amount|
+      if things == item
+        return amount
+      end
+    end
+    stock_of_item
+  end
+
+  def stock(item, amount)
+    if @inventory.key?(item)
+      @inventory[item] += amount
+    else
+      @inventory[item] = amount
+    end
+  end
+
+  
+end
--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -4,7 +4,9 @@ require './lib/food_truck'
 RSpec.describe Foodtruck do
   describe "Iteration 1" do
     before :each do
-      @food_truck = FoodTruck.new("Rocky Mountain Pies")
+      @food_truck = Foodtruck.new("Rocky Mountain Pies")
+      @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     end
 
     it "exists" do
@@ -12,9 +14,18 @@ RSpec.describe Foodtruck do
     end
 
     it "checks the attributes " do
-      expect(@food_truck.name).to eq("Apple Pie (Slice)")
+      expect(@food_truck.name).to eq("Rocky Mountain Pies")
       expect(@food_truck.inventory).to eq({})
-      expect(@food_truck.check_stock).to eq(0)
     end
-  end 
+
+    it "checks that count of the inventory" do
+      expect(@food_truck.check_stock(@item1)).to eq(0)
+      @food_truck.stock(@item1, 30)
+      expect(@food_truck.check_stock(@item1)).to eq(30)
+      @food_truck.stock(@item1, 25)
+      expect(@food_truck.check_stock(@item1)).to eq(55)
+      @food_truck.stock(@item2, 12)
+      expect(@food_truck.inventory).to eq({@item1 => 55, @item2 => 12})
+    end
+  end
 end
