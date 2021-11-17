42795ff79f8c9d49f5a002490901377285e88191
David Kassin <david.j.kassin@gmail.com>
2021-11-17 10:25:53 -0700

    Completed Iteration 2

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,6 @@
 class Item
 
-  attr_reader :name, :price, :stock
-
+  attr_reader :name, :price
   def initialize(details)
     @name = details[:name]
     @price = details[:price].delete("$").to_f
