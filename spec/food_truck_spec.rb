df722f6be8d18dcb05883ab36cdb43e6f58b11ff
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-17 09:56:12 -0700

    Test: add test for #potential_revenue method

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -40,4 +40,30 @@ RSpec.describe FoodTruck do
     food_truck.stock(item2, 12)
     expect(food_truck.inventory).to eq({item1 => 55, item2 => 12})
   end
+
+  xit 'can calculate potential revenue' do
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    #=> #<FoodTruck:0x00007fe1348a1160...>
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    #=> #<FoodTruck:0x00007fe1349bed40...>
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    #=> #<FoodTruck:0x00007fe134910650...>
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    #=> #<Item:0x007f9c56740d48...>
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    #=> #<Item:0x007f9c565c0ce8...>
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    #=> #<Item:0x007f9c562a5f18...>
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    #=> #<Item:0x007f9c56343038...>
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3.stock(item1, 65)
+
+    expect(food_truck1.potential_revenue).to eq(148.75)
+    expect(food_truck2.potential_revenue).to eq(345.00)
+    expect(food_truck3.potential_revenue).to eq(243.75)
+  end
 end
