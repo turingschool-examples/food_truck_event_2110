df83e7e7cbc1720cea422c878bc2274732f90cf0
David Kassin <david.j.kassin@gmail.com>
2021-11-17 09:15:55 -0700

    test: Show food truck class exists and has attributes

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,11 @@
+require './lib/item'
+require './lib/food_truck'
+describe FoodTruck do
+  it 'exists' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    expect(food_truck).to be_a FoodTruck
+    expect(food_truck.name).to eq ("Rocky Mountain Pies")
+    expect(food_truck.inventory).to eq ({})
+  end
+end
