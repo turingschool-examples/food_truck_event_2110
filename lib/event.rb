032747615d98d3c7c066b2d8e4588c2f61b7d182
John Napier <jcee.napier@gmail.com>
2021-11-17 11:55:02 -0700

    Update sell method in event

--- lib/event.rb
+++ lib/event.rb
@@ -73,4 +73,22 @@ class Event
       "#{date.slice(6..7)}/#{date.slice(4..5)}/#{date.slice(0..3)}"
     end 
   end
+
+  def sell(item, quantity)
+    return false if total_quantity_per_item(item) < quantity
+      
+    if total_quantity_per_item(item) > quantity
+      total_inventory.map do |item1, values|
+        values.map do |key, value|
+          if item == item1 && value.is_a?(Array)
+            value.map do |truck|
+              truck.inventory[item] - quantity
+            end
+          end
+        end
+        total_inventory
+      end
+    end 
+    true
+  end
 end
