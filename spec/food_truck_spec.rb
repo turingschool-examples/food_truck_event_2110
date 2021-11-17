6d66c8e085e1ab4ee9d1ecb5eb3a20e640743e86
Joel Grant <joelmgrant@me.com>
2021-11-17 09:12:38 -0700

    Test: check_stock and stock methods

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -4,6 +4,8 @@ require './lib/food_truck'
 describe FoodTruck do
   before(:each) do
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
   end
 
   describe '#initialize' do
@@ -20,5 +22,17 @@ describe FoodTruck do
     end
   end
 
+  describe '#check_stock' do
+    it 'returns the quantity of the chosen item' do
+      expect(@food_truck.check_stock(@item1)).to eq(0)
+    end
+  end
+
+  describe '#stock' do
+    it 'allows for an item to be stocked with a specific quantity' do
+      @food_truck.stock(@item1, 30)
+      expect(@food_truck.check_stock(@item1)).to eq(30)
+    end
+  end
   
 end
