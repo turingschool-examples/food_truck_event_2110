9b5ae9bbc46c9a0628eba9a3a7891acbe5520c12
Brad Breiten <jonathan@breiten.us>
2021-11-17 10:25:14 -0700

    Add initialize tests for Event class

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -3,5 +3,16 @@ require './lib/food_truck'
 require './lib/event'
 
 RSpec.describe Event do
-  
+  before(:each) do
+    @event = Event.new("South Pearl Street Farmers Market")
+  end
+
+  it 'exists' do
+    expect(@event).to be_a Event
+  end
+
+  it 'has attributes' do
+    expect(@event.name).to eq "South Pearl Street Farmers Market"
+    expect(@event.food_trucks).to eq([])
+  end
 end
