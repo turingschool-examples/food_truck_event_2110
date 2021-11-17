2731253ec64688d884d9019d89369a8b4fd4902c
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-11-17 09:17:19 -0700

    Complete #stock and #check_stock tests

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -4,6 +4,8 @@ require './lib/food_truck'
 describe FoodTruck do
 
   before(:each) do
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
   end
 
@@ -14,5 +16,19 @@ describe FoodTruck do
     end
   end
 
+  describe '#stock' do
+    it 'adds an item and its quantity to the inventory' do
+      @food_truck.stock(@item1, 30)
+      expect(@food_truck.inventory).to eq({@item1 => 30})
+    end
+  end
+
+  describe '#check_stock' do
+    it 'returns the quantity of an item' do
+      expect(@food_truck.check_stock(@item1)).to eq(0)
+      @food_truck.stock(@item1, 30)
+      expect(@food_truck.check_stock(@item1)).to eq(30)
+    end
+  end
 
 end
