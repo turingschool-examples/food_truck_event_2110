6664cdd658555d32e9f94a5e37ee4cd5d643f82b
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-17 11:24:14 -0700

    Test; add test for #sell items

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,6 +1,7 @@
 require './lib/item'
 require './lib/food_truck'
 require './lib/event'
+require 'date'
 require 'pry'
 
 RSpec.describe Event do
@@ -155,5 +156,33 @@ RSpec.describe Event do
 
     expected = ["Apple Pie (Slice)", "Banana Nice Cream", "Peach Pie (Slice)", "Peach-Raspberry Nice Cream"]
     expect(event.sorted_item_list).to eq(expected)
+  end
+
+  it 'can sell items' do
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    item5 = Item.new({name: 'Onion Pie', price: '$25.00'})
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    event = Event.new("South Pearl Street Farmers Market")
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+
+    food_truck3.stock(item1, 65)
+    food_truck3.stock(item3, 10)
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+
+    expect(event.sell(item1, 200)).to eq(false)
+    expect(event.sell(item5, 1)).to eq(false)
+    expect(event.sell(item4, 5)).to eq(true)
+    expect(event.sell(item1, 40)).to eq(true)
   end
 end
