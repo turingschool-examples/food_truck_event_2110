b13dfec5cc3edf6ec71620ecaa3a3d23f6ec51a8
Jessica Grazulis <jlgrazulis@gmail.com>
2021-11-17 09:41:17 -0700

    Add to stock test

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -20,9 +20,14 @@ describe Food_truck do
   it 'can stock items' do
     food_truck = Food_truck.new("Rocky Mountain Pies")
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     expect(food_truck.check_stock(item1)).to eq(0)
     food_truck.stock(item1, 30)
     expect(food_truck.inventory).to eq({item1 => 30})
+    food_truck.stock(item1, 25)
+    expect(food_truck.check_stock(item1)).to eq(55)
+    food_truck.stock(item2, 12)
+    expect(food_truck.inventory).to eq({item1 => 55, item2 => 12})
   end
 
 
