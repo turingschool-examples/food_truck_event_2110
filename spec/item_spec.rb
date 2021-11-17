25beed5e9af3a99d1a89aa7d20c05ab63505d330
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-17 09:12:31 -0700

    Feat: add attr_readers and instance variables for name and price

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,19 +1,19 @@
 require './lib/item'
 
 RSpec.describe Item do
-  xit 'exists' do
+  it 'exists' do
     item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
 
     expect(item2).to be_an_instance_of(Item)
   end
 
-  xit 'has a name' do
+  it 'has a name' do
     item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
 
     expect(item2.name).to eq("Apple Pie (Slice)")
   end
 
-  xit 'has a price' do
+  it 'has a price' do
     item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
 
     expect(item2.price).to eq(2.50)
