3045cbe2419501ad8d865d52fcf6b60b022a936c
Parker <parkerlockhart42@gmail.com>
2021-11-17 10:50:03 -0600

    Fix: instance variable references

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -12,11 +12,11 @@ describe Event do
     @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-    @food_truck1.stock(item1, 35)
-    @food_truck1.stock(item2, 7)
-    @food_truck2.stock(item4, 50)
-    @food_truck2.stock(item3, 25)
-    @food_truck3.stock(item1, 65)
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+    @food_truck3.stock(@item1, 65)
   end
 
   describe '#initialize' do
@@ -35,20 +35,20 @@ describe Event do
 
   describe 'add_food_truck' do
     it 'adds food trucks to array' do
-      @event.add_food_truck(food_truck1)
-      @event.add_food_truck(food_truck2)
-      @event.add_food_truck(food_truck3)
+      @event.add_food_truck(@food_truck1)
+      @event.add_food_truck(@food_truck2)
+      @event.add_food_truck(@food_truck3)
       expect(@event.food_trucks).to eq([@food_truck1, @food_truck2, @food_truck3])
     end
   end
 
   describe 'food_trucks_that_sell' do
     it 'gives a list of food trucks with item in stock' do
-      @event.add_food_truck(food_truck1)
-      @event.add_food_truck(food_truck2)
-      @event.add_food_truck(food_truck3)
+      @event.add_food_truck(@food_truck1)
+      @event.add_food_truck(@food_truck2)
+      @event.add_food_truck(@food_truck3)
       expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1, @food_truck3])
     end
-  end 
+  end
 
 end
