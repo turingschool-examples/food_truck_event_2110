ab822c5c7dc150985e53f119f1f373cca8150f8a
Jessica Grazulis <jlgrazulis@gmail.com>
2021-11-17 09:18:23 -0700

    Add Item initialize test

---
+++ spec/item_spec.rb
@@ -0,0 +1,10 @@
+require './lib/item'
+
+describe Item do
+  it 'exists' do
+     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+     item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+     expect(item1).to be_an_instance_of(Item)
+     expect(item2).to be_an_instance_of(Item)
+  end
+end
