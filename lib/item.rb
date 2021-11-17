55209b15e251a32255847b16a2703c5099176fe2
Wade Naughton <wadenaughton@gmail.com>
2021-11-17 11:27:04 -0500

    wow. price completed

--- lib/item.rb
+++ lib/item.rb
@@ -1,1 +1,15 @@
+class Item
 
+  attr_reader :name, :price
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @price = attributes[:price]
+  end
+
+  def price
+    test = @price.gsub(/\D/,'').to_f
+    test / 100
+  end
+
+end
