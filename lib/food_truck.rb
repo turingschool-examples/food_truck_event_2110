e509bb5088ca3d64f8374cce81ee97f02ffdb618
Brad Breiten <jonathan@breiten.us>
2021-11-17 09:22:09 -0700

    Complete IT1 for food truck

---
+++ lib/food_truck.rb
@@ -0,0 +1,20 @@
+class FoodTruck
+
+  attr_reader :name,
+              :inventory
+
+  def initialize(name)
+    @name = name
+    @inventory = Hash.new(0)
+  end
+
+  def stock(item, amount)
+    @inventory[item] += amount
+  end
+
+  def check_stock(item)
+    @inventory[item]
+  end
+
+
+end
