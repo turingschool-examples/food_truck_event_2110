f1773266bcd2328a08eb790b28263acb2e035a8f
Luke Pascale <lpluke5@gmail.com>
2022-01-19 10:56:38 -0700

    finalize testing on iteration 1

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -53,5 +53,25 @@ RSpec.describe FoodTruck do
     food_truck.stock(item1, 25)
     expect(food_truck.check_stock(item1)).to be(55)
     end
+
+    it 'can add to an existing stock of an item' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+    food_truck.stock(item2, 12)
+    expect(food_truck.check_stock(item2)).to be(12)
+    end
+
+    it 'can return the final updated hash' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+    food_truck.stock(item1, 30)
+    food_truck.stock(item1, 25)
+    food_truck.stock(item2, 12)
+    expect(food_truck.inventory).to eq({item1=>55, item2=>12})
+    end
   end
 end
