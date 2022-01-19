234f9cb27946d8df099ca8bc9daf6f6f2a14f738
Tyler Ross <tross020890@gmail.com>
2022-01-19 10:56:34 -0600

    feat: inventory holds items

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -28,13 +28,24 @@ RSpec.describe FoodTruck do
     expect(food_truck.check_stock(item1)).to eq(30)
   end
 
-  it "adds to inventory" do
+  it "adds item to inventory" do
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     food_truck.stock(item1, 30)
     food_truck.stock(item1, 25)
 
     expect(food_truck.check_stock(item1)).to eq(55)
+  end
+
+  it "holds multiple items" do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    food_truck.stock(item1, 30)
+    food_truck.stock(item1, 25)
+    food_truck.stock(item2, 12)
+
+    expect(food_truck.inventory.length).to eq(2)
   end
 
 end
