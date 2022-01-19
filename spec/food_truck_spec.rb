79e78f86ef2425a5bacb134935b40cf14a45be54
Michael Hicks <hicks.michael04@gmail.com>
2022-01-19 09:23:21 -0700

    test: add has attributes

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -4,8 +4,8 @@ require './lib/food_truck'
  RSpec.describe FoodTruck do
  	before(:each) do
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
-    # @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-    # @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
  	end
 
  	it 'exists' do
@@ -13,6 +13,7 @@ require './lib/food_truck'
  	end
 
  	it 'has attributes' do
- 		expect(@food_truck.attribute).to eq("Rocky Mountain Pies")
+ 		expect(@food_truck.name).to eq("Rocky Mountain Pies")
+ 		expect(@food_truck.inventory).to eq({})
  	end
  end
