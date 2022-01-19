f24e7cbec0ef1f3d6af33c83f143f4e401a45b50
benistta <92293363+benistta@users.noreply.github.com>
2022-01-19 09:39:26 -0700

    Feat: initialized class Event

---
+++ spec/event_spec.rb
@@ -0,0 +1,10 @@
+require './lib/item'
+require './lib/food_truck'
+require './lib/event'
+
+RSpec.describe Event do
+  it 'exists' do
+    event = Event.new("South Pearl Street Farmers Market")
+    expect(event).to be_instance_of(Event)
+  end
+end
