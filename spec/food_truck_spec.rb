3119de19752f333e12af9c0b407999dd87e0b1f7
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-19 10:12:41 -0600

    Test: FoodTruck initializes with a name

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,14 @@
+require_relative './spec_helper'
+
+RSpec.describe FoodTruck do
+  let (:food_truck) {FoodTruck.new("Rocky Mountain Pies")}
+
+  it 'exists' do
+    expect(food_truck). to be_instance_of FoodTruck
+  end
+
+  it 'initializes with a name' do
+    expect(food_truck.name).to eq("Rocky Mountain Pies")
+  end
+
+end
