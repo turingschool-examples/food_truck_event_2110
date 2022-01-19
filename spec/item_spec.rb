03ae0ca034b726fd36f4f1a05f0c9fc7f04bde28
Michael Hicks <hicks.michael04@gmail.com>
2022-01-19 09:21:11 -0700

    test: add has attributes

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -15,6 +15,6 @@ require './lib/item'
  		expect(@item1.name).to eq('Peach Pie (Slice)')
  		expect(@item1.price).to eq("$3.75")
  		expect(@item2.name).to eq('Apple Pie (Slice)')
- 		expect(@item2.price).to eq('$2.50')
+ 		expect(@item2.price).to eq('$2.50') 
  	end
  end
