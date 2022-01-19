50a5594dfca26ee2c9866efe1b5b1f54b7f4b3f4
Aedan Yturralde <aedanyturralde@gmail.com>
2022-01-19 09:50:37 -0700

    test: write can add food truck to event

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -11,10 +11,12 @@ RSpec.describe Event do
   let(:item3){Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})}
   let(:item4){item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})}
   let(:food_truck2){food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")}
-  # let{food_truck1.stock(item1, 35)}
-  # let{food_truck1.stock(item2, 7)}
-  # let(food_truck2.stock(item4, 50))
-  # food_truck2.stock(item3, 25)
+  let(:food_truck3){food_truck3 = FoodTruck.new("Palisade Peach Shack")}
+  let{food_truck1.stock(item1, 35)}
+  let{food_truck1.stock(item2, 7)}
+  let{food_truck2.stock(item4, 50)}
+  let{food_truck2.stock(item3, 25)}
+  let{food_truck3.stock(item1, 65)}
 
   it 'exists' do
     expect(event).to be_a(Event)
@@ -24,3 +26,11 @@ RSpec.describe Event do
     expect(event.name).to eq("South Pearl Street Farmers Market")
     expect(event.food_trucks).to eq([])
   end
+
+  it 'can add food_trucks to event' do
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    expect(event.food_trucks).to eq([food_truck1, food_truck2, food_truck3])
+  end
+end
