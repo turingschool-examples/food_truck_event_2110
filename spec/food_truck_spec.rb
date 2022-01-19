5918a5911342c6639663d5bf806e8de4c1dce633
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-19 11:14:48 -0500

    Test: create stocks and checks stock tests

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -2,6 +2,7 @@ require './lib/food_truck'
 RSpec.describe do FoodTruck
   context 'iteration1' do
     let(:food_truck) {food_truck = FoodTruck.new("Rocky Mountain Pies")}
+    let(:item1) {Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})}
 
 
     it 'exists' do
@@ -14,6 +15,17 @@ RSpec.describe do FoodTruck
 
     it 'inventory' do
       expect(food_truck.inventory).to eq({})
+    end
+
+    it 'checks stock' do
+      expect(food_truck.check_stock(item1)).to eq(0)
+    end
+
+    it 'stocks' do
+      food_truck.stock(item1, 30)
+      output = {item1 => 30}
+      expect(food_truck.inventory).to eq(output)
+      expect(food_truck.check_stock(item1)).to eq(30)
     end
   end
 end
