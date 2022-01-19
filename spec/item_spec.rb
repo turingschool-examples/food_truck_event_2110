3616f112da6f66d9f2abc1e90f8273f989b911a2
Joseph Galvin <jwgalvin@gmail.com>
2022-01-19 09:05:53 -0700

    test: item spec first tests added,for attributes.

---
+++ spec/item_spec.rb
@@ -0,0 +1,13 @@
+require './lib/item'
+require './lib/food_truck'
+require 'pry'
+require 'rspec'
+
+describe Item do
+  it "is a class with attributes" do
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    expect(item1.name).to eq("Apple Pie (Slice)")
+    expect(item2.price).to eq(2.5)
+  end
+end
