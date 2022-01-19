0e9bf72eefc663593073e8b34db2b6075f39ee5b
Conor <>
2022-01-19 09:40:03 -0700

    Test: food_trucks_that_sell

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -42,4 +42,8 @@ RSpec.describe "Iteration 2" do
     event.add_food_truck(food_truck3)
     expect(event.food_truck_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
   end
+
+  it "can list food_trucks that sell..." do
+    expect(event.food_trucks_that_sell(item1)).to eq([food_truck1, food_truck3])
+  end
 end
