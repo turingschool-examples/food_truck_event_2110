ffe34a7aa3406ce18f05236a0edebcc8cca741ca
Richard LaBrecque <RichardlaBreque@Gmail.com>
2022-01-19 11:05:28 -0500

    test: create item exists test

---
+++ spec/item_spec.rb
@@ -0,0 +1,11 @@
+require './lib/item'
+require 'pry'
+RSpec.describe Item do
+  before(:each) do
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+  end
+  it 'exists' do
+    expect(@item1).to be_a(Item)
+  end
+end
