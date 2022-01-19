682dda502a424d47174e553f480fe6752d31c0f4
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-19 10:36:52 -0600

    Test: Event can list food trucks by name

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -24,8 +24,17 @@ RSpec.describe Event do
     event.add_food_truck(food_truck1)
     event.add_food_truck(food_truck2)
     event.add_food_truck(food_truck3)
-    
+
     expect(event.food_trucks).to eq([food_truck1, food_truck2, food_truck3])
+  end
+
+  it 'can list the names of all food trucks' do
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    expect(event.food_trucks).to eq([food_truck1, food_truck2, food_truck3])
+
+    expect(event.food_truck_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
   end
 
 
