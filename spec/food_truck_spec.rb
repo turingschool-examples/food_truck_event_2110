9719feccd514ace39ce419a00fe0a143ed0f76e0
oscar santos <oscar303data@gmail.com>
2022-01-19 09:18:22 -0700

    Feat: create foodtruck exists and has attributes test block

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,15 @@
+require './lib/item'
+require './lib/food_truck'
+
+RSpec.describe FoodTruck do
+  it 'exists' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    expect(food_truck).to be_a(FoodTruck)
+  end
+
+  it 'has attributes' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    expect(food_truck.name).to eq("Rocky Mountain Pies")
+    expect(food_truck.inventory).to eq({})
+  end
+end
