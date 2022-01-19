bebe7ca4ebfe2b7e6fea3d288dfe2a6451abbb62
Joseph Galvin <jwgalvin@gmail.com>
2022-01-19 09:20:17 -0700

    feat: foodtruck passes first test.

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -11,10 +11,10 @@ describe FoodTruck do
 
   end
 
-  it "interacts with other classes" do
+  xit "interacts with other classes" do
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-    expect(food_truck.check_stock(item1).to eq(0)
+    expect(food_truck.check_stock(item1)).to eq(0)
   end
 end
