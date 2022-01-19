2d7ffc434d3f7b293223abaecd608ba690e209f5
Aedan Yturralde <aedanyturralde@gmail.com>
2022-01-19 10:34:40 -0700

    test: write #total_inventory test

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -63,4 +63,34 @@ RSpec.describe Event do
     expect(food_truck3.potential_revenue).to eq(243.75)
   end
 
+  it '#total_inventory' do
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3.stock(item1, 65)
+    food_truck3.stock(item3, 10)
+    expect(event.total_inventory).to eq({
+       item1 => {
+        quantity: 100,
+        food_trucks: [food_truck1, food_truck3]
+      },
+        item2 => {
+        quantity: 7,
+        food_trucks: [food_truck1]
+      },
+        item_4 => {
+         quantity: 50,
+         food_trucks: [food_truck2]
+      },
+        item_3 => {
+          quantity: 35,
+          food_trucks: [food_truck2, food_truck3]
+      },
+    })
+  end
+
 end
