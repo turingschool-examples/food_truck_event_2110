26063eeebaa2e7ce8cfa7cf6026d3cee57760ca5
Jakob Edelstein <jakobedelstein@gmail.com>
2022-01-19 09:37:46 -0700

    Event is also initialized with an empty array of Food Trucks

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -4,8 +4,12 @@ RSpec.describe Event do
   before(:each) do
     @event = Event.new("South Pearl Street Farmers Market")
   end
-  it 'is initialized with a name' do
-    expect(@event).to be_instance_of Event
+  it 'is initialized with a name and an empty collection of food trucks' do
+    expect(@event).to be_an Event
+    expect(@event.name).to be_a String
     expect(@event.name).to eq "South Pearl Street Farmers Market"
+    expect(@event.food_trucks).to be_an Array
+    expect(@event.food_trucks).to be_empty
+
   end
 end
