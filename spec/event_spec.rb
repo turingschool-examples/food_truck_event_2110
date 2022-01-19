8fbbd69ce2a62f48d6406dc92efddd6cbd113b5d
Jimmy Page <jpage@knights.ucf.edu>
2022-01-19 10:44:57 -0700

    made tests for total inventory method

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -52,4 +52,37 @@ describe Event do
     expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1, @food_truck3])
     expect(@event.food_trucks_that_sell(@item4)).to eq([@food_truck2])
   end
+
+  it 'can tell you total inventory' do
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+    @food_truck3.stock(@item1, 65)
+    @food_truck3.stock(@item3, 10)
+    expected = {
+     @item1 => {
+       quantity: 100,
+       food_trucks: [@food_truck1, @food_truck3]
+     },
+     @item2 => {
+       quantity: 7,
+       food_trucks: [@food_truck1]
+     },
+     @item4 => {
+       quantity: 50,
+       food_trucks: [@food_truck1]
+     },
+     @item3 => {
+       quantity: 35,
+       food_trucks: [@food_truck2, @food_truck3]
+     },
+   }
+
+    expect(@event.total_inventory).to eq(expected)
+  end
+
 end
