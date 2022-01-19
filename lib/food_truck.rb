0608e4d1b44152e901307b0dbd1691cc55fe8615
benistta <92293363+benistta@users.noreply.github.com>
2022-01-19 09:17:44 -0700

    Feat: has a name test and method works

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,5 +1,6 @@
 class FoodTruck
-
+  attr_reader :name
+  
   def initialize(name)
     @name = name
   end
--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe FoodTruck do
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     expect(food_truck).to be_instance_of(FoodTruck)
   end
+
+  it 'has a name' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    expect(food_truck.name).to eq("Rocky Mountain Pies")
+  end
 end
