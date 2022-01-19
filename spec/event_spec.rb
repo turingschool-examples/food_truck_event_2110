a3056b2e1baf7a382eaa5a420324b0db5fcc7912
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-01-19 10:20:41 -0700

    Feat: Event class initialized, tests pass

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -15,11 +15,11 @@ RSpec.describe Event do
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
 
-    @food_truck1.stock(item1, 35)
-    @food_truck1.stock(item2, 7)
-    @food_truck2.stock(item4, 50)
-    @food_truck2.stock(item3, 25)
-    @food_truck3.stock(item1, 65)
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+    @food_truck3.stock(@item1, 65)
 
 	end
 
