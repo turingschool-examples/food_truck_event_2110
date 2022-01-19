63ed549dc1c6363512e084525df5af9d30f58c3a
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-19 11:24:22 -0500

    Feat: create stocks and stocks twice method

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -5,4 +5,16 @@ class FoodTruck
     @name = name
     @inventory = Hash.new(0)
   end
+
+  def check_stock(item)
+    if @inventory.key?(item) == false
+      0
+    else
+      @inventory[item]
+    end
+  end
+
+  def stock(item, amount)
+    @inventory[item] += amount
+  end
 end
