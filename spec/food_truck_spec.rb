a28d9522c8187d149ad031468dfb5a7fa807dd0c
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-19 11:17:46 -0500

    Test: create stocks twice and multiple items tests

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe do FoodTruck
   context 'iteration1' do
     let(:food_truck) {food_truck = FoodTruck.new("Rocky Mountain Pies")}
     let(:item1) {Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})}
-
+    let(:item2) {Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})}
 
     it 'exists' do
       expect(food_truck).to be_a(FoodTruck)
@@ -26,6 +26,20 @@ RSpec.describe do FoodTruck
       output = {item1 => 30}
       expect(food_truck.inventory).to eq(output)
       expect(food_truck.check_stock(item1)).to eq(30)
+    end
+
+    it 'stocks twice' do
+      food_truck.stock(item1, 30)
+      food_truck.stock(item1, 25)
+      expect(food_truck.check_stock(item1)).to eq(55)
+    end
+
+    it 'stocks multiples' do
+      food_truck.stock(item1, 30)
+      food_truck.stock(item1, 25)
+      food_truck.stock(item2, 12)
+      output = {item1 => 30, item2 => 12}
+      expect(food_truck.inventory.to eq(output)
     end
   end
 end
