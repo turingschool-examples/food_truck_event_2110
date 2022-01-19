d477607b4cd455ef6804fbf2f1d6e3ed5f77d64b
Lisa Taylor <ltaylor.lisa@gmail.com>
2022-01-19 09:19:25 -0700

    Fix: Test correction and passing initial tests for FoodTruck

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -6,6 +6,7 @@ describe FoodTruck do
   before(:each) do
     @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
   end
 
   it 'exists' do
