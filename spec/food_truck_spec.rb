81bd2004819973166a78ba7baa097971a6c10d96
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-19 10:26:13 -0600

    Test: FoodTruck can stock multiple items to Inventory and return stock when requested

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -3,6 +3,7 @@ require_relative './spec_helper'
 RSpec.describe FoodTruck do
   let (:food_truck) {FoodTruck.new("Rocky Mountain Pies")}
   let (:item1) {Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})}
+  let (:item2) {Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})}
 
   it 'exists' do
     expect(food_truck). to be_instance_of FoodTruck
@@ -24,6 +25,19 @@ RSpec.describe FoodTruck do
 
     expect(food_truck.inventory).to eq({item1 => 30})
     expect(food_truck.check_stock(item1)).to eq(30)
+  end
+
+  it 'can stock muliple items to inventory and check the stock' do
+    expect(food_truck.inventory).to eq({})
+
+    food_truck.stock(item1, 30)
+    expect(food_truck.inventory).to eq({item1 => 30})
+
+    food_truck.stock(item1, 25)
+    expect(food_truck.check_stock(item1)).to eq(55)
+
+    food_truck.stock(item2, 12)
+    expect(food_truck.inventory).to eq({item1 => 55, item2 => 12})
   end
 
 end
