5d8f68a996440bbd27a302a704ab3328969515c9
Jimmy Page <jpage@knights.ucf.edu>
2022-01-19 09:20:29 -0700

    hash creation working to stock items

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -5,4 +5,13 @@ class FoodTruck
     @name = name
     @inventory = Hash.new(0)
   end
-end 
+
+  def stock(item_object, number_of_item)
+    @inventory[item_object] += number_of_item
+  end
+
+  def check_stock(item_object)
+    @inventory.values_at(item_object).join.to_i
+  end
+  
+end
