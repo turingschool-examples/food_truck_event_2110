d4de0a115734f483ee64f2dd2125aa35803f5590
Joseph Galvin <jwgalvin@gmail.com>
2022-01-19 09:41:25 -0700

    Feat: food truck can have inventory of item classes.

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -6,4 +6,17 @@ class FoodTruck
     @inventory = Hash.new(0)
   end
 
+  def check_stock(item)
+    # @inventory.select do |inven|
+    #    if inven == item
+    #      binding.pry
+         @inventory[item]
+    #    end
+    # end
+  end
+
+  def stock(item, amount)
+    @inventory[item] += amount
+  end
+
 end
