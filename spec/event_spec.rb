b1362988b019bf7606151d5134e8448a542c6e41
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-19 11:05:58 -0600

    Test: Event can calculate total inventory of all food trucks

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -51,6 +51,34 @@ RSpec.describe Event do
     expect(event.food_trucks_that_sell(item4)).to eq([food_truck2])
   end
 
+  it 'can calculate total inventory of all food trucks at event' do
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3.stock(item1, 65)
+    food_truck3.stock(item3, 10)
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+
+    expected = {
+      item1 => {
+        quantity: 100,
+        food_trucks: [food_truck1, food_truck3]},
+      item2 => {
+        quantity: 7,
+        food_trucks: [food_truck1]},
+      item4 => {
+        quantity: 50,
+        food_trucks: [food_truck2]},
+      item3 => {
+        quantity: 35,
+        food_trucks: [food_truck2, food_truck3]}}
+
+    expect(event.total_inventory).to eq(expected)
+  end
+
 
 
 end
