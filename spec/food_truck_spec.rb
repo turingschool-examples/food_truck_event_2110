ed263d1144c571288ea1b1c61ef636e94d104962
Sulaiman Birashk <sully.birashk@gmail.com>
2022-01-19 08:22:14 -0800

    Iteration one complete

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -41,6 +41,13 @@ RSpec.describe FoodTruck do
     expect(@food_truck.check_stock(@item1)).to eq(55)
   end
 
+  it "can add inventory" do
+    @food_truck.stock(@item1, 30)
+    @food_truck.stock(@item1, 25)
+    @food_truck.stock(@item2, 12)
+    expect(@food_truck.inventory).to eq({@item1 => 55, @item2 => 12})
+  end
+
 
 
 
