ed9a1a395ecde9ab95a838ab30ece773623d3e15
Sam Smith <samlsmith424@gmail.com>
2022-01-19 08:54:19 -0800

    Test: #potential_revenue

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -28,4 +28,23 @@ RSpec.describe FoodTruck do
     food_truck.stock(item2, 12)
     expect(food_truck.inventory).to eq({item1 => 55, item2 => 12})
   end
+
+  describe 'iteration 2' do
+    let(:food_truck1) {FoodTruck.new("Rocky Mountain Pies")}
+    let(:item3) {Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})}
+    let(:item4) {Item.new({name: "Banana Nice Cream", price: "$4.25"})}
+    let(:food_truck2) {FoodTruck.new("Ba-Nom-a-Nom")}
+    let(:food_truck3) {FoodTruck.new("Palisade Peach Shack")}
+
+    it '#potential_revenue' do
+      food_truck1.stock(item1, 35)
+      food_truck1.stock(item2, 7)
+      food_truck2.stock(item4, 50)
+      food_truck2.stock(item3, 25)
+      food_truck3.stock(item1, 65)
+      expect(food_truck1.potential_revenue).to eq(148.75)
+      expect(food_truck2.potential_revenue).to eq(345.00)
+      expect(food_truck3.potential_revenue).to eq(243.75)
+    end
+  end
 end
