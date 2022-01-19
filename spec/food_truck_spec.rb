2fcbfb87486332efcc51b6e008a9ca9d3110d746
Lisa Taylor <ltaylor.lisa@gmail.com>
2022-01-19 09:17:15 -0700

    Feat: add initial tests for FoodTruck

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,8 +1,19 @@
-# require './lib/food_truck'
-# require 'rspec'
-#
-# describe FoodTruck do
-#   before(:each) do
-#     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-#     item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-#   end
+require './lib/food_truck'
+require 'rspec'
+
+######## ITERATION 1 #########
+describe FoodTruck do
+  before(:each) do
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+  end
+
+  it 'exists' do
+    expect(@food_truck).to be_instance_of(FoodTruck)
+  end
+
+  it 'has attributes' do
+    expect(@food_truck.name).to eq("Rocky Mountain Pies")
+    expect(@food_truck.inventory).to eq({})
+  end
+end
