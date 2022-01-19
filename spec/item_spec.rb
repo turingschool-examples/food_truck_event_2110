2c1d2bb9e68a7a58bea878b5a4841ab2eefd6324
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-19 11:05:29 -0500

    Test: create name and price test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,9 +2,18 @@ require './lib/item'
 RSpec.describe do Item
   context 'iteration1' do
     let(:item1) {Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})}
+    let(:item2) {Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})}
 
     it 'exists' do
       expect(item1).to be_a(Item)
+    end
+
+    it 'has a name' do
+      expect(item2.name).to eq("Apple Pie (Slice)")
+    end
+
+    it 'has a name' do
+      expect(item2.price).to be(2.50)
     end
   end
 end
