981a278829976866e46935a33b4fc4e6dea68f6f
Conor <>
2022-01-19 09:22:08 -0700

    Test: stock

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -18,4 +18,14 @@ RSpec.describe "Iteration 1" do
   it "can check stock" do
     expect(food_truck.check_stock(item1)).to eq(0)
   end
+
+  it "can stock" do
+    food_truck.stock(item1, 30)
+    expect(food_truck.inventory).to eq({item1 => 30})
+    expect(food_truck.check_stock(item1)).to eq(30)
+    food_truck.stock(item1, 25)
+    expect(food_truck.check_stock(item1)).to eq(55)
+    food_truck.stock(item2, 12)
+    expect(food_truck.inventory).to eq({item1 => 55, item2 => 12})
+  end
 end
