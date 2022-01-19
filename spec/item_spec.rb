6e9de5fcc4196d8005282fe6ce693f2b832cf23e
Alicia Watt <alicia.watt.aw@gmail.com>
2022-01-19 09:20:05 -0700

    test:  item class

---
+++ spec/item_spec.rb
@@ -0,0 +1,16 @@
+require './lib/item'
+
+describe Item do
+  before (:each) do
+    @item = Item.new({name: 'Peach Pie (Slice)', price: "3.75"})
+  end
+  describe 'initialize' do
+    it 'exists' do
+      expect(@item).to be_a(Item)
+    end
+    it 'has_attributes' do
+      expect(@item.name).to eq('Peach Pie (Slice)')
+      expect(@item.price).to eq("3.75")
+    end
+  end
+end
