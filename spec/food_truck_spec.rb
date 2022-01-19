9c1e30f774cfb4f6866e02394b49427628d88ed0
Luke Pascale <lpluke5@gmail.com>
2022-01-19 09:27:06 -0700

    feat: Add inventory hash to FoodTruck class.

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -15,5 +15,11 @@ RSpec.describe FoodTruck do
 
     expect(food_truck.name).to eq("Rocky Mountain Pies")
     end
+
+    it 'has inventory as a hash' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    expect(food_truck.inventory).to eq({})
+    end
   end
 end
