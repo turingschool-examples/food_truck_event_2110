9eea16188e1fd69fc90d0f771775646309fac548
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-19 11:35:03 -0500

    Test: create sell item and sell diff item tests

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -50,5 +50,31 @@ RSpec.describe do Event
       output = ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
       expect(event.food_trucks).to eq(output)
     end
+
+    it 'gives trucks that sell item' do
+      food_truck1.stock(item1, 35)
+      food_truck1.stock(item2, 7)
+      food_truck2.stock(item4, 50)
+      food_truck2.stock(item3, 25)
+      food_truck3.stock(item1, 65)
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+      output1 = [food_truck1, food_truck3]
+      expect(event.food_trucks_that_sell(item1)).to eq(output)
+    end
+
+    it 'gives trucks that sell DIFF item' do
+      food_truck1.stock(item1, 35)
+      food_truck1.stock(item2, 7)
+      food_truck2.stock(item4, 50)
+      food_truck2.stock(item3, 25)
+      food_truck3.stock(item1, 65)
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+      output1 = [food_truck2]
+      expect(event.food_trucks_that_sell(item4)).to eq(output)
+    end
   end
 end
