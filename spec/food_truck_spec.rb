66f40f9f18198b36ba57f1e386b97e2e39e98440
Michael Hicks <hicks.michael04@gmail.com>
2022-01-19 09:47:38 -0700

    test: add can add/remove stock and count quantity of an item in stock

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -28,6 +28,10 @@ require './lib/food_truck'
 
     @food_truck.stock(@item1, 25)
     expect(@food_truck.check_stock(@item1)).to eq(55)
+
+    @food_truck.stock(@item2, 12)
+    expect(@food_truck.inventory).to eq(@item1 => 55, @item2 => 12)
+
   end
 
 
