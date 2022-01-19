e0045b9de37b03f4ee1b8a07c748356dfc3692fd
TJ Williams <williamsjr.timothy1@gmail.com>
2022-01-19 11:26:08 -0500

    Feat: FoodTruck can #check_stock

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,8 +1,14 @@
+require 'pry'
+
 class FoodTruck
   attr_reader :name, :inventory
 
   def initialize(name)
     @name = name
     @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    @inventory[item]
   end
 end
