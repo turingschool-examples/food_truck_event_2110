d75ca439e20e70187c87e1891b798def31385f52
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-19 10:17:51 -0600

    Test: FoodTruck can stock items into inventory and check stock

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -2,6 +2,7 @@ require_relative './spec_helper'
 
 RSpec.describe FoodTruck do
   let (:food_truck) {FoodTruck.new("Rocky Mountain Pies")}
+  let (:item1) {Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})}
 
   it 'exists' do
     expect(food_truck). to be_instance_of FoodTruck
@@ -13,6 +14,16 @@ RSpec.describe FoodTruck do
 
   it 'begins with no inventory' do
     expect(food_truck.inventory).to eq({})
+  end
+
+  it 'can stock items in inventory and check the stock' do
+    expect(food_truck.inventory).to eq({})
+    expect(food_truck.check_stock(item1)).to eq(0)
+
+    food_truck.stock(item1, 30)
+
+    expect(food_truck.inventory).to eq({item1 => 30})
+    expect(food_truck.check_stock(item1)).to eq(30)
   end
 
 end
