7c7a9b2e1a5d525a9344c51266652a1ed4e67cbe
Tyler Ross <tross020890@gmail.com>
2022-01-19 10:46:42 -0600

    feat: food truck stocks and checks stock of items

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,7 +1,15 @@
 class FoodTruck
-  attr_reader :name
+  attr_reader :name, :inventory
   def initialize(name)
     @name = name
-    
+    @inventory = {}
+  end
+
+  def check_stock(item)
+    @inventory[item]
+  end
+
+  def stock(item, quantity)
+    @inventory[item] = quantity
   end
 end
--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,4 +1,5 @@
 require './lib/food_truck'
+require './lib/item'
 RSpec.describe FoodTruck do
 
   it "exists" do
@@ -18,4 +19,13 @@ RSpec.describe FoodTruck do
 
     expect(food_truck.inventory).to be_instance_of(Hash)
   end
+
+  it "can have inventory" do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    food_truck.stock(item1, 30)
+
+    expect(food_truck.check_stock(item1)).to eq(30)
+  end
+
 end
