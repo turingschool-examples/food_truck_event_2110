7a1e567cf8e57dd90de79d81611e56e8b9d42ac5
Jakob Edelstein <jakobedelstein@gmail.com>
2022-01-19 09:27:13 -0700

    feat: Food Truck contains an inventory, and can write to and read from the inventory

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,4 +1,5 @@
 require './lib/food_truck'
+require './lib/item'
 
 RSpec.describe FoodTruck do
   before(:each) do
@@ -8,4 +9,36 @@ RSpec.describe FoodTruck do
     expect(@food_truck).to be_instance_of FoodTruck
     expect(@food_truck.name).to eq "Rocky Mountain Pies"
   end
- end
+
+  it 'is initialized with an empty inventory hash' do
+    expect(@food_truck.inventory).to be_a Hash
+    expect(@food_truck.inventory).to be_empty
+  end
+
+  describe '#check_stock' do
+    before :each do
+      @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    end
+
+    it 'can check the stock of an item, stock will default to 0' do
+      expect(@food_truck.check_stock(@item1)).to be_an Integer
+      expect(@food_truck.check_stock(@item1)).to be 0
+    end
+
+    describe '#stock' do
+      before :each do
+        @food_truck.stock(@item1, 30)
+      end
+
+      it 'takes an item object and a quantity as args, and places the object in inventory' do
+        expected = { @item1 => 30 }
+        expect(@food_truck.inventory).to eq expected
+      end
+
+      it '#check_stock will return the updated value' do
+        expect(@food_truck.check_stock(@item1)).to be_an Integer
+        expect(@food_truck.check_stock(@item1)).to be 30
+      end
+    end
+  end
+end
