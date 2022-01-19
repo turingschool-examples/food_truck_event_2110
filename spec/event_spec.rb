1d39d1e1836a229f49166d3a6c354926e71674a5
Sam Smith <samlsmith424@gmail.com>
2022-01-19 08:47:20 -0800

    Test: #food_truck_names and #food_trucks_that_sell

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -14,4 +14,46 @@ RSpec.describe Event do
     expect(event.food_trucks).to eq([])
   end
 
+  describe 'iteration 2' do
+    let(:food_truck1) {FoodTruck.new("Rocky Mountain Pies")}
+    let(:item1) {Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})}
+    let(:item2) {Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})}
+    let(:item3) {Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})}
+    let(:item4) {Item.new({name: "Banana Nice Cream", price: "$4.25"})}
+    let(:food_truck2) {FoodTruck.new("Ba-Nom-a-Nom")}
+    let(:food_truck3) {FoodTruck.new("Palisade Peach Shack")}
+
+    it 'can stock more items' do
+      food_truck1.stock(item1, 35)
+      food_truck1.stock(item2, 7)
+      food_truck2.stock(item4, 50)
+      food_truck2.stock(item3, 25)
+      food_truck3.stock(item1, 65)
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+      expect(event.food_trucks).to eq([food_truck1, food_truck2, food_truck3])
+    end
+
+    it '#food_truck_names' do
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+      expect(event.food_truck_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+    end
+
+    it '#food_trucks_that_sell a specific item' do
+      food_truck1.stock(item1, 35)
+      food_truck1.stock(item2, 7)
+      food_truck2.stock(item4, 50)
+      food_truck2.stock(item3, 25)
+      food_truck3.stock(item1, 65)
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+      expect(event.food_trucks_that_sell(item1)).to eq([food_truck1, food_truck3])
+      expect(event.food_trucks_that_sell(item4)).to eq([food_truck2])
+    end
+  end
+
 end
