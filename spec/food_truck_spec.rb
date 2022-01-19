6fdde6d0e587c44336f20f59af9de641ce98b1a4
benistta <92293363+benistta@users.noreply.github.com>
2022-01-19 09:36:25 -0700

    Feat: Methods stock and check stock work well

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -29,4 +29,29 @@ RSpec.describe FoodTruck do
     food_truck.stock(item1, 30)
     expect(food_truck.inventory).to eq({item1 => 30})
   end
+
+  it 'checks stocks' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    food_truck.stock(item1, 30)
+    expect(food_truck.check_stock(item1)).to eq(30)
+  end
+
+  it 'checks stocks' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    food_truck.stock(item1, 30)
+    food_truck.stock(item1, 25)
+    expect(food_truck.check_stock(item1)).to eq(55)
+  end
+
+  it 'checks inventory' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    food_truck.stock(item1, 30)
+    food_truck.stock(item1, 25)
+    food_truck.stock(item2, 12)
+    expect(food_truck.inventory).to eq({item1 => 55, item2 => 12})
+  end
 end
