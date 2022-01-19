47be2158463298583905b0789620bdf11a0a73e5
benistta <92293363+benistta@users.noreply.github.com>
2022-01-19 09:14:12 -0700

    Feat: has a price test and method passes

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -14,4 +14,10 @@ RSpec.describe Item do
     item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     expect(item2.name).to eq("Apple Pie (Slice)")
   end
+
+  it 'item2 has a price' do
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    expect(item2.price).to eq(2.50)
+  end
 end
