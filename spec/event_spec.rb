93d62d8fda897af0db54acefe9c4e2ad6d3a5acc
Maximilian Wagner <maw3350@gmail.com>
2022-01-19 13:01:24 -0500

    test: add test for #total_inventory

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -113,5 +113,21 @@ RSpec.describe Event do
       event.add_food_truck(food_truck3)
       expect(event.sorted_item_list).to eq(["Apple Pie (Slice)", "Banana Nice Cream", "Peach Pie (Slice)", "Peach-Raspberry Nice Cream"])
     end
+
+    it '#total_inventory' do
+
+      food_truck1.stock(item1, 35)
+      food_truck1.stock(item2, 7)
+      food_truck2.stock(item4, 50)
+      food_truck2.stock(item3, 25)
+      food_truck3.stock(item1, 65)
+      food_truck3.stock(item3, 10)
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+      expect(event.total_inventory).to be_a(Hash)
+      expect(event.total_inventory.count).to eq(4)
+      expect(event.total_inventory.keys).to eq([item1, item2, item3, item4])
+    end
   end
 end
