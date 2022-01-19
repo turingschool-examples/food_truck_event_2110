7e21928131c02712557d96d8292519a62fc6dc89
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-19 10:06:14 -0600

    Test: Item initializes with a name and a price

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,1 +1,16 @@
+require_relative './spec_helper'
 
+RSpec.describe Item do
+  let (:item1) {Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})}
+  let (:item2) {Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})}
+
+  it 'exists' do
+    expect(item2).to be_instance_of Item
+  end
+
+  it 'initializes with a name and price' do
+    expect(item2.name).to eq("Apple Pie (Slice)")
+    expect(item2.price).to eq(2.50)
+  end
+
+end
