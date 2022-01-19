57255e6d8d6d470a51dd93d484435aa228bbfce8
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-19 10:38:40 -0600

    Add #food_truck_names to list all food trucks added to Event by name

--- lib/event.rb
+++ lib/event.rb
@@ -9,6 +9,10 @@ class Event
 
   def add_food_truck(food_truck)
     @food_trucks << food_truck
-  end 
+  end
+
+  def food_truck_names
+    @food_trucks.map {|food_truck| food_truck.name}
+  end
 
 end
