31d3fb6ca6370aeab045a7296c41f213510ffe1d
Jesse Ochs <91091690+JesseOchs@users.noreply.github.com>
2022-01-19 11:56:28 -0700

    Feat: list names

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -12,27 +12,27 @@ describe Event do
     @item4 = Item.new({ name: 'Banana Nice Cream', price: '$4.25' })
   end
 
-  xit 'exists and has attributes' do
+  it 'exists and has attributes' do
     expect(@event).to be_a(Event)
     expect(@event.name).to eq('South Pearl Street Farmers Market')
     expect(@event.food_trucks).to eq([])
   end
 
-  xit 'can add food trucks' do
+  it 'can add food trucks' do
     @event.add_food_truck(@food_truck1)
     @event.add_food_truck(@food_truck2)
     @event.add_food_truck(@food_truck3)
-    expect(event.food_trucks).to eq([@food_truck1, @food_truck2, @food_truck3])
+    expect(@event.food_trucks).to eq([@food_truck1, @food_truck2, @food_truck3])
   end
 
-  xit 'can name the food trucks ' do
+  it 'can name the food trucks ' do
     @event.add_food_truck(@food_truck1)
     @event.add_food_truck(@food_truck2)
     @event.add_food_truck(@food_truck3)
     expect(@event.food_truck_names).to eq(['Rocky Mountain Pies', 'Ba-Nom-a-Nom', 'Palisade Peach Shack'])
   end
 
-  xit 'can list food trucks that sell an item ' do
+  it 'can list food trucks that sell an item ' do
     @food_truck1.stock(@item1, 35)
     @food_truck1.stock(@item2, 7)
     @food_truck2.stock(@item4, 50)
