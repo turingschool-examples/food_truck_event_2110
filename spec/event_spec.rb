1e3513b971d81e5e8b3c24fb550047b3a6846195
Jimmy Page <jpage@knights.ucf.edu>
2022-01-19 09:44:29 -0700

    wrote test for list food truck names

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -33,5 +33,19 @@ describe Event do
     expect(@event.food_trucks).to eq([@food_truck1, @food_truck2, @food_truck3])
   end
 
+  it 'can list food truck names' do
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+    expect(@event.food_truck_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+  end
+
+
+
+  # @food_truck1.stock(@item1, 35)
+  # food_truck1.stock(item2, 7)
+  # food_truck2.stock(item4, 50)
+  # food_truck2.stock(item3, 25)
+  # food_truck3.stock(item1, 65)
 
 end
