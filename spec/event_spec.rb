d368679e2498e870ce368960b928aa10dd6b2ba0
Richard LaBrecque <RichardlaBreque@Gmail.com>
2022-01-19 11:53:11 -0500

    feat event exists

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -11,13 +11,13 @@ RSpec.describe Event do
     @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-    @food_truck1.stock(item1, 35)
-    @food_truck1.stock(item2, 7)
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
     @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
-    @food_truck2.stock(item4, 50)
-    @food_truck2.stock(item3, 25)
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
     @food_truck3 = FoodTruck.new("Palisade Peach Shack")
-    @food_truck3.stock(item1, 65)
+    @food_truck3.stock(@item1, 65)
     end
 
   it 'exists' do
