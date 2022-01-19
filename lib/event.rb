6b4f300574c77a454688b475493c598fa6e99e47
Aedan Yturralde <aedanyturralde@gmail.com>
2022-01-19 11:33:38 -0700

    define #item_sum_at_event helper method to use in total_inventory method. #total_inventory passes test, but hash syntax is slightly differerent from interaction pattern. Still a hash at least.

--- lib/event.rb
+++ lib/event.rb
@@ -20,4 +20,32 @@ class Event
       food_truck.inventory.include?(item)
     end
   end
+
+  def item_sum_at_event(item)
+    array_to_sum = []
+    food_trucks.each do |food_truck|
+      if food_truck.inventory.include?(item)
+        food_truck.inventory.each do |stock_item, quantity|
+          if item == stock_item then  array_to_sum << quantity
+          end
+        end
+      end
+    end
+    array_to_sum.sum
+  end
+
+  def total_inventory
+    hash = {}
+    food_trucks.each do |food_truck|
+      food_truck.inventory.each do |item, inventory|
+        sub_hash = {}
+        if hash.has_key?(item) == false
+        sub_hash[:quantity] = item_sum_at_event(item)
+        sub_hash[:food_trucks] = food_trucks_that_sell(item)
+        hash[item] = sub_hash
+        end
+      end
+    end
+    hash
+  end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -73,22 +73,23 @@ RSpec.describe Event do
     food_truck2.stock(item3, 25)
     food_truck3.stock(item1, 65)
     food_truck3.stock(item3, 10)
+    expect(event.item_sum_at_event(item1)).to eq(100)
     expect(event.total_inventory).to eq({
        item1 => {
-        quantity: 100,
-        food_trucks: [food_truck1, food_truck3]
+        :quantity => 100,
+        :food_trucks => [food_truck1, food_truck3]
       },
         item2 => {
-        quantity: 7,
-        food_trucks: [food_truck1]
+        :quantity => 7,
+        :food_trucks => [food_truck1]
       },
-        item_4 => {
-         quantity: 50,
-         food_trucks: [food_truck2]
+        item4 => {
+         :quantity => 50,
+         :food_trucks => [food_truck2]
       },
-        item_3 => {
-          quantity: 35,
-          food_trucks: [food_truck2, food_truck3]
+        item3 => {
+          :quantity => 35,
+          :food_trucks => [food_truck2, food_truck3]
       },
     })
   end
