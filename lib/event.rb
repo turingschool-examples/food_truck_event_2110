bb34f8ef7b8b83685bdf10026c67e16da4eff568
Jimmy Page <jpage@knights.ucf.edu>
2022-01-19 09:49:00 -0700

    wrote method for listing food truck name

--- lib/event.rb
+++ lib/event.rb
@@ -9,4 +9,10 @@ class Event
   def add_food_truck(food_truck_object)
     @food_trucks << food_truck_object
   end
+
+  def food_truck_names
+    @food_trucks.map do |food_truck|
+      food_truck.name
+    end
+  end
 end
