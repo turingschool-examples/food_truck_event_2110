c0149c719b868bc459a8f40c77e8d8a130030d15
Tyler Ross <tross020890@gmail.com>
2022-01-19 10:38:27 -0600

    feat: Food truck exists

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,10 @@
+require './lib/food_truck'
+RSpec.describe FoodTruck do
+
+  it "exists" do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    expect(food_truck).to be_instance_of(FoodTruck)
+  end
+
+end
