05f16f1431b1eb5e998ef366081e89061e241252
Sam Iavri <sammi80134@gmail.com>
2022-01-19 11:48:55 -0700

    cleaned up tests

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -21,27 +21,13 @@ RSpec.describe '#event' do
   it 'has  food_trucks' do
     event = Event.new('South Pearl Street Farmers Market')
 
-    item1 = Item.new({ name: 'Peach Pie (Slice)', price: '$3.75' })
-    item2 = Item.new({ name: 'Apple Pie (Slice)', price: '$2.50' })
-    item3 = Item.new({ name: 'Peach-Raspberry Nice Cream', price: '$5.30' })
-    item4 = Item.new({ name: 'Banana Nice Cream', price: '$4.25' })
-    expect(item1).to be_a(Item)
-    expect(item2).to be_a(Item)
-    expect(item3).to be_a(Item)
-    expect(item4).to be_a(Item)
-
     food_truck1 = FoodTruck.new('Rocky Mountain Pies')
-    food_truck1.stock(item1, 35)
-    food_truck1.stock(item2, 7)
     expect(food_truck1).to be_a(FoodTruck)
 
     food_truck2 = FoodTruck.new('Ba-Nom-a-Nom')
-    food_truck2.stock(item4, 50)
-    food_truck2.stock(item3, 25)
     expect(food_truck2).to be_a(FoodTruck)
 
     food_truck3 = FoodTruck.new('Palisade Peach Shack')
-    food_truck3.stock(item1, 65)
     expect(food_truck3).to be_a(FoodTruck)
 
     event.add_food_truck(food_truck1)
@@ -54,27 +40,13 @@ RSpec.describe '#event' do
   it 'has a list food_truck by name' do
     event = Event.new('South Pearl Street Farmers Market')
 
-    item1 = Item.new({ name: 'Peach Pie (Slice)', price: '$3.75' })
-    item2 = Item.new({ name: 'Apple Pie (Slice)', price: '$2.50' })
-    item3 = Item.new({ name: 'Peach-Raspberry Nice Cream', price: '$5.30' })
-    item4 = Item.new({ name: 'Banana Nice Cream', price: '$4.25' })
-    expect(item1).to be_a(Item)
-    expect(item2).to be_a(Item)
-    expect(item3).to be_a(Item)
-    expect(item4).to be_a(Item)
-
     food_truck1 = FoodTruck.new('Rocky Mountain Pies')
-    food_truck1.stock(item1, 35)
-    food_truck1.stock(item2, 7)
     expect(food_truck1).to be_a(FoodTruck)
 
     food_truck2 = FoodTruck.new('Ba-Nom-a-Nom')
-    food_truck2.stock(item4, 50)
-    food_truck2.stock(item3, 25)
     expect(food_truck2).to be_a(FoodTruck)
 
     food_truck3 = FoodTruck.new('Palisade Peach Shack')
-    food_truck3.stock(item1, 65)
     expect(food_truck3).to be_a(FoodTruck)
 
     event.add_food_truck(food_truck1)
