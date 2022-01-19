ba8a7f05fa18cf8af60000efcc3ef34dc54dc0ca
Sulaiman Birashk <sully.birashk@gmail.com>
2022-01-19 08:11:02 -0800

    Feat: FoodTruck Class Test: attributes

---
+++ lib/food_truck.rb
@@ -0,0 +1,12 @@
+require 'pry'
+
+class FoodTruck
+  attr_reader :name
+  attr_accessor
+
+  def initialize(name)
+    @name = name
+  end
+
+
+end
--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -3,11 +3,15 @@ require 'pry'
 
 RSpec.describe FoodTruck do
   before :each do
-
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
   end
 
   it "exists" do
-    expect().to be_instance_of
+    expect(@food_truck).to be_instance_of(FoodTruck)
+  end
+
+  it "has a name" do
+    expect(@food_truck.name).to eq("Rocky Mountain Pies")
   end
 
 end
