4862887a4ad6d89a81a11eaa526bbefa74e75c04
Luke Pascale <lpluke5@gmail.com>
2022-01-19 11:29:23 -0700

    feat: Add ability to read just the food truck names at the event.

--- lib/event.rb
+++ lib/event.rb
@@ -1,5 +1,5 @@
 class Event
-  attr_reader :name, :food_trucks
+  attr_reader :name, :food_trucks, :food_truck_names
   def initialize(name)
     @name = name
     @food_trucks = []
@@ -7,5 +7,14 @@ class Event
 
   def add_food_truck(truck)
     @food_trucks << truck
-  end 
+  end
+
+  def food_truck_names
+    # binding.pry
+    names = []
+    @food_trucks.map do |food_truck|
+      names << food_truck.name
+    end
+    names
+  end
 end
