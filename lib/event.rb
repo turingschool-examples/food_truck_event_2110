ce87ecfc6fa3c12d706b4a00d3c5fecdc72482ab
Tyler Ross <tross020890@gmail.com>
2022-01-19 11:23:24 -0600

    feat: lists food truck names

--- lib/event.rb
+++ lib/event.rb
@@ -1,8 +1,18 @@
 class Event
-  attr_reader :name
+  attr_reader :name, :food_trucks
   def initialize(name)
     @name = name
+    @food_trucks = []
 
+  end
+
+  def add_food_truck(food_truck)
+    @food_trucks << food_truck
+    #require 'pry';binding.pry
+  end
+
+  def food_truck_names
+    @food_trucks.map {|truck| truck.name}
   end
 
 end
